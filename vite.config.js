5f5b23a7b529088eb26f77b1bba927bd264aaf80
baimingxuan <354372738@qq.com>
2021-12-31 23:53:55 +0800

    Initial project

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 8888
+  },
+  plugins: [vue()]
+})
