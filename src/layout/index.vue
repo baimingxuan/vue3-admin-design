20df1b003d145c98e57105f5f39d48a71e285ce1
baimingxuan <354372738@qq.com>
2022-01-07 09:50:12 +0800

    feat: add layout

---
+++ src/layout/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <ElContainer>
+    <ElAside width="200px">Aside</ElAside>
+    <ElContainer>
+      <ElHeader>Header</ElHeader>
+      <ElMain>Main</ElMain>
+      <ElFooter>Footer</ElFooter>
+    </ElContainer>
+  </ElContainer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ElAside, ElContainer, ElHeader, ElMain, ElFooter } from 'element-plus'
+
+export default defineComponent({
+  name: 'Layout',
+  components: {
+    ElAside,
+    ElContainer,
+    ElHeader,
+    ElMain,
+    ElFooter
+  }
+})
+</script>
+
+<style lang="less">
+
+</style>
