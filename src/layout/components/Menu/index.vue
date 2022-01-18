7af96ab9222962e0f8f9cedf3f55c6b8b3c88bd6
baimingxuan <354372738@qq.com>
2022-01-18 22:44:28 +0800

    feat: develop menu component

--- src/layout/components/Menu/index.vue
+++ src/layout/components/Menu/index.vue
@@ -1,3 +1,21 @@
 <template>
-
+  <Menu :mode="menuMode"
+        :collapse="menuCollapse">
+  </Menu>
 </template>
+
+<script lang="ts">
+  import { ElMenu as Menu } from 'element-plus'
+  import { defineComponent, ref } from 'vue'
+  export default defineComponent({
+    components: { Menu },
+    setup() {
+      const menuMode = ref('vertical')
+      const menuCollapse = ref(false)
+      return {
+        menuMode,
+        menuCollapse
+      }
+    }
+  })
+</script>
