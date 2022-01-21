8ce43e007b6c7a149772e131b76be9ceddbf01af
baimingxuan <354372738@qq.com>
2022-01-21 16:41:56 +0800

    feat: develop linkItem component

---
+++ src/layout/components/Menu/LinkItem.vue
@@ -0,0 +1,34 @@
+<script lang="tsx">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'LinkItem',
+    props: {
+      toPath: {
+        type: String,
+        required: true
+      }
+    },
+    setup(props) {
+      /**
+       * determining external link or route
+       * @param path
+       */
+      function isExternal(path: string): boolean {
+        return /^(https?:|mailto:|tel:)/.test(path)
+      }
+
+      return () => {
+        return (
+            <>
+              {isExternal(props.toPath) ? (
+                <a href={props.toPath} target='_blank' rel='noopener' />
+              ) : (
+                <router-link to={props.toPath} />
+              )}
+            </>
+        )
+      }
+    }
+  })
+</script>
--- src/layout/components/Menu/index.vue
+++ src/layout/components/Menu/index.vue
@@ -1,19 +1,29 @@
 <template>
   <Menu :mode="menuMode"
-        :collapse="menuCollapse">
+        :collapse="menuCollapse"
+        :unique-opened="true"
+        :background-color="menuBgColor"
+        :text-color="menuTextColor"
+        :active-text-color="menuActiveTextColor">
   </Menu>
 </template>
 
 <script lang="ts">
   import { ElMenu as Menu } from 'element-plus'
   import type { MenuState } from './types'
+  import MenuContent from './MenuContent.vue'
   import { defineComponent, toRefs, reactive } from 'vue'
+
   export default defineComponent({
-    components: { Menu },
+    name: 'LayoutMenu',
+    components: { Menu, MenuContent },
     setup() {
       const menuState = reactive<MenuState>({
         menuMode: 'vertical',
-        menuCollapse: false
+        menuCollapse: false,
+        menuBgColor: '#263238',
+        menuTextColor: '#fff',
+        menuActiveTextColor: '#409eff'
       })
       return {
         ...toRefs(menuState)
