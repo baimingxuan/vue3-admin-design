85930d67b991d6aff3e7e556abb0a4b9045a77a4
baimingxuan <354372738@qq.com>
2022-01-23 22:30:27 +0800

    feat: configure store

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'element-plus/theme-chalk/index.css'
 import { setupRouter } from './router'
+import { setupStore } from '@/store'
 
 function launchApp() {
     const app = createApp(App)
+
+    // Configure router
     setupRouter(app)
+
+    // Configure store
+    setupStore(app)
+
     app.mount('#app')
 }
 
