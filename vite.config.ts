70e023438954d74d7ae4be97cfba2358e69d9b28
baimingxuan <354372738@qq.com>
2022-03-28 18:36:04 +0800

    pref: add windi css plugin to handle multiple screens

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import WindiCSS from 'vite-plugin-windicss'
 // need install plugin @types/node -> yarn add @types/node -D
 import { resolve } from 'path'
 
@@ -14,7 +15,7 @@ export default defineConfig({
     host: true,
     port: 8888
   },
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), WindiCSS()],
   resolve: {
     alias: [
       {
