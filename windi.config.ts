7b83af8ebdc480103ecdbedeed81eb6619383222
baimingxuan <354372738@qq.com>
2022-03-29 19:19:22 +0800

    pref: import windi css plugin

--- windi.config.ts
+++ windi.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from 'vite-plugin-windicss'
 
 export default defineConfig({
     darkMode: 'class',
+    extract: {
+        include: ['**/*.{vue,html,jsx,tsx,ts}'],
+        exclude: ['node_modules', '.git', 'dist', '.DS_Store', '.idea', '.vscode', 'yarn.lock', 'windi.config.{ts,js}'],
+    },
     theme: {
-        screens: {
-            'sm': '640px',
-            'md': '768px',
-            'lg': '1024px',
-            'xl': '1280px',
-            '2xl': '1600px',
+        extend: {
+            screens: {
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+                '2xl': '1600px',
+            }
         }
     }
 })
