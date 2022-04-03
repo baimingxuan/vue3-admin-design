b762c3b4feb06353d58ac335c2569bcf9da33a29
baimingxuan <354372738@qq.com>
2022-04-03 23:33:32 +0800

    feat: add setting component

---
+++ src/layout/components/Settings/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <Button type="primary" @click="showDrawer">Open</Button>
+  <Drawer
+      v-model:visible="visible"
+      class="custom-class"
+      style="color: red"
+      title="Basic Drawer"
+      placement="right"
+      @after-visible-change="afterVisibleChange"
+  >
+  </Drawer>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { Drawer, Button } from 'ant-design-vue'
+
+export default defineComponent({
+  name: 'Setting',
+  components: { Drawer, Button },
+  setup() {
+    const visible = ref<boolean>(false);
+
+    const afterVisibleChange = (bool: boolean) => {
+      console.log('visible', bool);
+    };
+
+    const showDrawer = () => {
+      visible.value = true;
+    };
+
+    return {
+      visible,
+      afterVisibleChange,
+      showDrawer,
+    };
+  },
+})
+</script>
+
