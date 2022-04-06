3b6617399a2cd847455b67096195adcdb080a1b2
baimingxuan <354372738@qq.com>
2022-04-06 22:21:39 +0800

    feat: import dynamic theme from antd

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <RouterView />
+  <ConfigProvider>
+    <RouterView />
+  </ConfigProvider>
 </template>
 
 <script setup lang="ts">
-
+  import { ConfigProvider } from 'ant-design-vue'
 </script>
 
 <style lang="less">
