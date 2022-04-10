67a7d9246ceabe6436158d25edb2b04183d9084e
baimingxuan <354372738@qq.com>
2022-04-10 22:18:45 +0800

    feat: refactor sider by using antd

---
+++ src/layouts/sider/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Sider
+    collapsible>
+  </Sider>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Layout } from 'ant-design-vue'
+
+  export default defineComponent({
+    components: { Sider: Layout.Sider }
+  })
+</script>
