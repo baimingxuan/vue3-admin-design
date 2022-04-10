b5017f414bf498cf5c47615cdb082efec8db6c40
baimingxuan <354372738@qq.com>
2022-04-10 22:03:03 +0800

    feat: refactor the layout using antd

---
+++ src/layouts/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Layout></Layout>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Layout } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'BasicLayout',
+    components: { Layout }
+  })
+</script>
