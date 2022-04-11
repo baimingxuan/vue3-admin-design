c7c285d2d68ff7b1323cd38a55ed8311b1c3f79d
baimingxuan <354372738@qq.com>
2022-04-11 23:05:20 +0800

    feat: use propTypes in menu

--- src/layouts/menu/index.vue
+++ src/layouts/menu/index.vue
@@ -1,7 +1,16 @@
 <script lang="tsx">
   import { defineComponent } from 'vue'
+  import { propTypes } from '@/utils/propTypes'
 
   export default defineComponent({
-    name: 'LayoutMenu'
+    name: 'LayoutMenu',
+    props: {
+      menuTheme: propTypes.oneOf(['light', 'dark']),
+      
+      menuMode: {
+        type: String,
+        default: ''
+      }
+    }
   })
 </script>
