6a1bdd3eac5b164dcd6d2ef341173bc10fc3c5f3
baimingxuan <354372738@qq.com>
2022-04-12 23:11:41 +0800

    feat: modify types module name to declares

---
+++ src/declares/global.d.ts
@@ -0,0 +1,5 @@
+declare interface ViteEnv {
+    VITE_PORT: number;
+    VITE_PUBLIC_PATH: string;
+    VITE_PROXY: [string, string][];
+}
---
+++ src/declares/module.d.ts
@@ -0,0 +1,5 @@
+declare module '*.vue' {
+    import { DefineComponent } from 'vue'
+    const Component: DefineComponent<{}, {}, any>
+    export default Component
+}
