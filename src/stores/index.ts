f87eb0b3013efc7409f7f5db16970db3fdb4f565
baimingxuan <354372738@qq.com>
2022-04-12 22:36:55 +0800

    feat: modify store module name

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,8 @@
 import type { App } from 'vue'
 import { createPinia } from 'pinia'
 
-const store = createPinia()
+const stores = createPinia()
 
 export function setupStore(app: App<Element>) {
-    app.use(store)
+    app.use(stores)
 }
