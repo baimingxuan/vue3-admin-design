f9b8c3d6d074fa24938f413d998334d3fe470c0c
baimingxuan <354372738@qq.com>
2022-04-13 23:44:34 +0800

    feat: update useMenuSetting.ts

--- src/hooks/setting/useMenuSetting.ts
+++ src/hooks/setting/useMenuSetting.ts
@@ -1,5 +1,20 @@
-
+import { computed } from 'vue';
+import { useAppStore } from '@/stores/modules/app';
 
 export function useMenuSetting() {
-  
+  const appStore = useAppStore()
+
+  const getMenuType = computed(() => appStore.getMenuSetting.type)
+
+  const getMenuMode = computed(() => appStore.getMenuSetting.mode)
+
+  const getMenuTheme = computed(() => appStore.getMenuSetting.theme)
+
+  const getMenuFold = computed(() => appStore.getMenuSetting.menuFold)
+
+  const getCollapsed = computed(() => appStore.getMenuSetting.collapsed)
+
+  const getCollapsedShowTitle = computed(() => appStore.getMenuSetting.collapsedShowTitle)
+
+  const getMenuWidth = computed(() => appStore.getMenuSetting.menuWidth)
 }
