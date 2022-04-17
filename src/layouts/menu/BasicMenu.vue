c637286f3c29a34c82221834a801d614d9de1eff
baimingxuan <354372738@qq.com>
2022-04-17 22:39:40 +0800

    feat: add basicMenu component

---
+++ src/layouts/menu/BasicMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <Menu
+    :subMenuOpenDelay="0.2"
+  >
+  </Menu>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Menu } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'BasicMenu',
+    components: {
+      Menu
+    },
+  })
+</script>
