9ad37bb38e5ac5f5f35dfbd1eaa805e80a9b5f9b
baimingxuan <354372738@qq.com>
2022-04-18 22:02:35 +0800

    add the SubMenuItem component to the Menu module

---
+++ src/layouts/menu/components/SubMenuItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <MenuItem v-if="!menuHasChildren(item) && getShowMenu" />
+  <AntdSubMenu
+    v-if="menuHasChildren(item) && getShowMenu"
+    :key="`submenu-${item.path}`"
+  >
+    <template #title>
+      <MenuItemContent :item="item" />
+    </template>
+
+    <template v-for="childrenItem in item.children || []" :key="childrenItem.path">
+      <SubMenuItem :item="childrenItem" />
+    </template>
+  </AntdSubMenu>
+</template>
+
+<script lang="ts">
+  import { defineComponent, computed } from 'vue'
+  import { Menu as AntdMenu } from 'ant-design-vue'
+  import type { Menu as MenuType } from '@/router/types'
+  import MenuItem from './MenuItem.vue'
+  import MenuItemContent from './MenuItemContent.vue'
+  
+  export default defineComponent({
+    name: 'SubMenuItem',
+    components: {
+      MenuItem,
+      MenuItemContent,
+      AntdSubMenu: AntdMenu.SubMenu
+    },
+    props: {
+      item: {
+        type: Object,
+        default: {}
+      }
+    },
+    setup(props) {
+      const getShowMenu = computed(() => !props.item.meta?.hideMenu)
+
+      function menuHasChildren(menuTreeItem: MenuType): boolean {
+        return (
+          !menuTreeItem.meta?.hideChildrenInMenu &&
+          Reflect.has(menuTreeItem, 'children') &&
+          !!menuTreeItem.children &&
+          menuTreeItem.children.length > 0
+        )
+      }
+
+      return {
+        getShowMenu,
+        menuHasChildren
+      }
+    }
+  })
+</script>
