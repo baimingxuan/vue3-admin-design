f81d121dcbf02ba1990e1654ea6490be5b5ecc0f
baimingxuan <354372738@qq.com>
2022-04-18 21:30:38 +0800

    feat: add the MenuItem component to the Menu module

---
+++ src/layouts/menu/components/MenuItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <AntdMenuItem :key="item.path"></AntdMenuItem>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Menu as AntdMenu } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'MenuItem',
+    components: { AntdMenuItem: AntdMenu.Item },
+    props: {
+      item: {
+        type: Object,
+        default: {}
+      }
+    }
+  })
+</script>
