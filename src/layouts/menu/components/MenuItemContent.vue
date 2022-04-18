2a300ba11ff4cf7aa6a753f02ca79503fc10cd3c
baimingxuan <354372738@qq.com>
2022-04-18 21:37:55 +0800

    feat: add the MenuItemContent component to the Menu module

---
+++ src/layouts/menu/components/MenuItemContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <span></span>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'MenuItemContent'
+  })
+</script>
