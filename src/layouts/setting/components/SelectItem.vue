bd719055e5a9b4abdf21b484f773879f21ba7038
baimingxuan <354372738@qq.com>
2022-04-19 21:26:11 +0800

    feat: add selectItem component to setting module

---
+++ src/layouts/setting/components/SelectItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="select-item">
+    <span> {{ title }}</span>
+    <AntdSelect />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, computed } from 'vue'
+
+  import { Select as AntdSelect } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'SelectItem',
+    components: { AntdSelect },
+    props: {
+      title: {
+        type: String,
+        default: ''
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      options: {
+        type: Array,
+        default: () => []
+      }
+    }
+  })
+</script>
+
+<style lang="less">
+  .select-item {
+    display: flex;
+    justify-content: space-between;
+    margin: 16px 0;
+
+    &-select {
+      width: 120px
+    }
+  }
+</style>
