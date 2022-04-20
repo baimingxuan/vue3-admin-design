a2068527c5d7bd154aca7612c6e4311a9ea03a1a
baimingxuan <354372738@qq.com>
2022-04-20 22:56:31 +0800

    feat: add ImportAsyncComponent.tsx

---
+++ src/utils/factory/ImportAsyncComponent.tsx
@@ -0,0 +1,33 @@
+import { defineAsyncComponent } from 'vue'
+import { Spin as AntdSpin } from 'ant-design-vue'
+
+interface Fn<T = any, R = T> {
+  (...arg: T[]): R;
+}
+
+interface Options {
+  size?: 'default' | 'small' | 'large';
+  delay?: number;
+  timeout?: number;
+  loading?: boolean;
+  retry?: boolean;
+}
+
+export function ImportAsyncComponent(loader: Fn, options: Options = {}) {
+  const { size = 'small', delay = 100, timeout = 5000, loading = false, retry = true } = options
+  return defineAsyncComponent({
+    loader,
+    loadingComponent: loading ? <AntdSpin spinning={true} size={size} /> : undefined,
+    timeout,
+    delay,
+    onError: !retry
+      ? () => {}
+      : (error, retry, fail, attempts) => {
+        if (error.message.match(/fetch/) && attempts <= 3) {
+          retry()
+        } else {
+          fail();
+        }
+      }
+  })
+}
