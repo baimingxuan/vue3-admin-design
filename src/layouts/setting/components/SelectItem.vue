20586d5797eed91d1cf8aba4bf35f850bd6bb75c
baimingxuan <354372738@qq.com>
2022-04-20 22:02:56 +0800

    feat: optimization of SelectItem components

--- src/layouts/setting/components/SelectItem.vue
+++ src/layouts/setting/components/SelectItem.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="select-item">
     <span> {{ title }}</span>
-    <AntdSelect />
+    <AntdSelect
+      size="small"
+      v-bind="getValue"
+      :options="options"
+      :disabled="disabled"
+    />
   </div>
 </template>
 
 <script lang="ts">
   import { defineComponent, computed } from 'vue'
+  import type { PropType } from 'vue'
 
   import { Select as AntdSelect } from 'ant-design-vue'
+  import type { SelectOptions } from '@/types'
 
   export default defineComponent({
     name: 'SelectItem',
@@ -22,9 +29,21 @@
         type: Boolean,
         default: false
       },
+      def: {
+        type: [String, Number] as PropType<string | number>
+      },
       options: {
-        type: Array,
+        type: Array as PropType<SelectOptions>,
         default: () => []
+      }
+    },
+    setup(props) {
+      const getValue = computed(() => {
+        return props.def ? { value: props.def } : {}
+      })
+
+      return {
+        getValue
       }
     }
   })
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,9 @@
 export type ThemeMode = 'dark' | 'light'
 
 export type LocaleType = 'zh_CN' | 'en'
+
+export type SelectOptions = {
+  label: string
+  value: any
+  [key: string]: string | number | boolean
+}[]
