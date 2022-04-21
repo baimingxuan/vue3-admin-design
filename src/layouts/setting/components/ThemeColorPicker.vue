e8a08dd802599205f01306e194cf5474ef2e6b50
baimingxuan <354372738@qq.com>
2022-04-21 20:34:10 +0800

    feat: add ThemeColorPicker component

---
+++ src/layouts/setting/components/ThemeColorPicker.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="compo_theme-color-picker">
+    <template v-for="color in colorList || []" :key="color"></template>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import type { PropType } from 'vue'
+  export default defineComponent({
+    name: 'ThemeColorPicker',
+    props: {
+      colorList: {
+        type: Array as PropType<any>,
+        default: []
+      }
+    }
+  })
+</script>
+
+<style lang="less" scoped>
+
+</style>
