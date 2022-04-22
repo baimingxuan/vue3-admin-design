8072279fda6b8808a3d0499571cb9dcfb5c61a6f
baimingxuan <354372738@qq.com>
2022-04-22 18:38:30 +0800

    feat: add MenuTypePicker component

---
+++ src/layouts/setting/components/MenuTypePicker.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="compo_menu-type-picker">
+    <template v-for="item in menuTypeList || []" :key="item.title">
+      <Tooltip :title="item.title" placement="top">
+        <div></div>
+      </Tooltip>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import type { PropType } from 'vue'
+
+  export default defineComponent({
+    name: 'MenuTypePicker',
+    props: {
+      menuTypeList: {
+        type: Array as PropType<any>,
+        default: () => []
+      },
+      def: {
+        type: String,
+        default: '',
+      }
+    }
+  })
+</script>
+
+<style lang="less" scoped>
+.compo_menu-type-picker {
+  display: flex;
+}
+</style>
