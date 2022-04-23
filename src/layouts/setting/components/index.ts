0e7dfec76e48abb4bf6b8b3fbbc212f9d84a08a1
baimingxuan <354372738@qq.com>
2022-04-23 22:33:00 +0800

    feat: the setting component is exported asynchronously

--- src/layouts/setting/components/index.ts
+++ src/layouts/setting/components/index.ts
@@ -3,3 +3,5 @@ import { ImportAsyncComponent } from '../../../utils/factory/ImportAsyncComponen
 export const SwitchItem = ImportAsyncComponent(() => import('./SwitchItem.vue'))
 export const SelectItem = ImportAsyncComponent(() => import('./SelectItem.vue'))
 export const InputNumItem = ImportAsyncComponent(() => import('./InputNumItem.vue'))
+export const MenuTypePicker = ImportAsyncComponent(() => import('./MenuTypePicker.vue'))
+export const ThemeColorPicker = ImportAsyncComponent(() => import('./ThemeColorPicker.vue'))
