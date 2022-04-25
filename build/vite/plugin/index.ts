3b928774041b4e7162a71f87ec96006b5440f217
baimingxuan <354372738@qq.com>
2022-04-25 18:22:31 +0800

    feat: add vite-plugin-svg-icons plugin

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -2,6 +2,7 @@ import type { Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import windiCSS from 'vite-plugin-windicss'
+import { configSvgIconsPlugin } from './svgIcons'
 
 export function createVitePlugins(isBuild: boolean) {
     const vitePlugins: (Plugin | Plugin[])[] = [
@@ -12,6 +13,9 @@ export function createVitePlugins(isBuild: boolean) {
 
     // vite-plugin-windicss
     vitePlugins.push(windiCSS())
+
+    // vite-plugin-svg-icons
+    vitePlugins.push(configSvgIconsPlugin(isBuild))
 
     return vitePlugins
 }
---
+++ build/vite/plugin/svgIcons.ts
@@ -0,0 +1,15 @@
+/**
+ *  vite plugin for importing svg-icons
+ *  https://github.com/vbenjs/vite-plugin-svg-icons
+ */
+
+  import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+  import { resolve } from 'path'
+
+  export function configSvgIconsPlugin(isBuild: boolean) {
+    return createSvgIconsPlugin({
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+      svgoOptions: isBuild,
+      symbolId: 'icon-[dir]-[name]'
+    })
+  }
