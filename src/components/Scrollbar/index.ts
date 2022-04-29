8e7ab4f680907427bd755579cb6728fb5f171a61
baimingxuan <354372738@qq.com>
2022-04-29 16:46:41 +0800

    feat: export Scrollbar component

--- src/components/Scrollbar/index.ts
+++ src/components/Scrollbar/index.ts
@@ -2,3 +2,7 @@
  * @description referenced from element-ui/scrollbar
  * @link see: https://github.com/ElemeFE/element/tree/dev/packages/scrollbar
  */
+
+import Scrollbar from './src/main.vue'
+
+export default Scrollbar
