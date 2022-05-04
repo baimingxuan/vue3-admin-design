7888ec128df3c80ba276a42a250febb109264705
baimingxuan <354372738@qq.com>
2022-05-04 22:32:58 +0800

    feat: update is.ts

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -13,3 +13,11 @@ export function isArray(val: any): val is Array<any> {
 export function isObject(val: any): val is Record<any, any> {
     return val !== null && is(val, 'Object')
 }
+
+export function isDef<T = unknown>(val?: T): val is T {
+    return typeof val !== 'undefined';
+}
+
+export function isUnDef<T = unknown>(val?: T): val is T {
+    return !isDef(val);
+}
