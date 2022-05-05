1b3cd614cb3cc0fa1f9ee4b1999fec9f79b1573d
baimingxuan <354372738@qq.com>
2022-05-05 22:37:57 +0800

    feat: add footer component

---
+++ src/layouts/footer/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <AntdFooter>
+    <div>Copyright &copy;2022 baimingxuan</div>
+  </AntdFooter>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+  import { Layout } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'LayoutFooter',
+    components: { AntdFooter: Layout.Footer }
+  })
+</script>
+
+<style lang="less" scoped>
+
+</style>
