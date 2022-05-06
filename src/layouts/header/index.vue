bc8697a6db4853fc4337934fc4cc877ed6c2ef92
baimingxuan <354372738@qq.com>
2022-05-06 22:26:13 +0800

    feat: add header component

---
+++ src/layouts/header/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <AntdHeader></AntdHeader>
+</template>
+
+<script lang="ts">
+  import { defineComponent, computed } from 'vue'
+  import { Layout } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'LayoutHeader',
+    components: { AntdHeader: Layout.Header }
+  })
+</script>
