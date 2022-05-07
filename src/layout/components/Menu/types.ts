fbbdd08851b13248e889853783b29e27d3d4d11f
baimingxuan <354372738@qq.com>
2022-05-07 18:19:14 +0800

    pref: delete layout module

--- src/layout/components/Menu/types.ts
+++
@@ -1,9 +0,0 @@
-import { MenuModeEnum } from '@/enums/menuEnum'
-
-export interface MenuState {
-    menuMode: MenuModeEnum,
-    menuCollapse: boolean,
-    menuBgColor: string,
-    menuTextColor: string,
-    menuActiveTextColor: string
-}
