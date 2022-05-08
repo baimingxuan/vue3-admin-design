b4b5b1a40c97c8064bdbbea3d9514c922cbabc98
baimingxuan <354372738@qq.com>
2022-05-08 23:05:18 +0800

    feat: update layout component

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,8 +1,5 @@
 <template>
   <Layout>
-    <div class="side-menu">
-      <Menu :items="$router.options.routes"></Menu>
-    </div>
     <Setting></Setting>
   </Layout>
 </template>
@@ -11,23 +8,9 @@
   import { defineComponent, onMounted } from 'vue'
   import { Layout } from 'ant-design-vue'
   import Setting from './setting/index.vue'
-  import Menu from './menu/components/Menu.vue'
 
   export default defineComponent({
     name: 'BasicLayout',
-    components: { Layout, Setting, Menu },
-    mounted() {
-      console.log(this.$router.options.routes)
-    }
+    components: { Layout, Setting }
   })
 </script>
-
-<style lang="less">
-.side-menu {
-  position: fixed;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  width: 210px;
-}
-</style>
