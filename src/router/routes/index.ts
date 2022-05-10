7a6703227b8cdbe85705f915a99acba720f52ce9
baimingxuan <354372738@qq.com>
2022-05-10 20:33:34 +0800

    feat: update router/index

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,16 +2,22 @@ import type { AppRoute } from '../types'
 import Layout from '../../layout/index.vue'
 import { asyncRoutes } from './routeList'
 
+/**
+ * meta: {
+    title: 'title'              the name show in menu and breadcrumb (recommend set)
+    icon: 'icon-name'           the icon show in the menu
+    affix: false                if set true, the tag will affix in the tags-view (default is false)     
+    hideMenu: false             if set true, menu item will hide in the menu (default is false)
+    hideChildrenInMenu: false   if set true, menu children will hide in the menu (default is false)
+    orderNo: 0                  menu item display sequence
+  }
+ * */
+
 export const HomeRoute: AppRoute = {
     path: '/',
     name: 'Home',
     component: Layout,
     redirect: '/home',
-    meta: {
-        title: '首页',
-        icon: 'vue-dsn-icon-index',
-        hideChildrenInMenu: false
-    },
     children: [
         {
             path: 'home',
@@ -20,9 +26,7 @@ export const HomeRoute: AppRoute = {
             meta: {
                 title: '首页',
                 icon: 'vue-dsn-icon-index',
-                fixed: true,
-                hideMenu: false,
-                hideChildrenInMenu: false
+                affix: true
             }
         }
     ]
