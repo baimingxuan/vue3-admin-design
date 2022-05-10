4317353b562b1e1b5eb139847e572c5552b1ccd7
baimingxuan <354372738@qq.com>
2022-05-10 15:28:09 +0800

    feat: add redirect component

---
+++ src/views/redirect.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+  import { unref } from 'vue'
+  import { useRouter } from 'vue-router'
+
+  const { currentRoute, replace } = useRouter()
+
+  const { params, query } = unref(currentRoute)
+  const { path, redirect_type = 'path' } = params
+
+  Reflect.deleteProperty(params, 'path')
+  Reflect.deleteProperty(params, 'redirect_type')
+
+  const pathStr = Array.isArray(path) ? path.join('/') : path
+
+  if (redirect_type === 'name') {
+    replace({
+      name: pathStr,
+      params,
+      query
+    })
+  } else {
+    replace({
+      path: pathStr.startsWith('/') ? pathStr : '/' + pathStr,
+      query 
+    })
+  }
+</script>
