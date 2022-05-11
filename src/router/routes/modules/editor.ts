e87b20e8a7ae4651002c761f490992321cebc171
baimingxuan <354372738@qq.com>
2022-05-11 20:37:37 +0800

    feat: add editor route

---
+++ src/router/routes/modules/editor.ts
@@ -0,0 +1,31 @@
+import type { AppRoute } from '../../types'
+import Layout from '@/layout/index.vue'
+
+export const ImageRoute: AppRoute = {
+    path: '/editor',
+    name: 'Editor',
+    component: Layout,
+    redirect: '/editor/markdown',
+    meta: {
+        title: '编辑器',
+        icon: 'vue-dsn-icon-bianjiqi'
+    },
+    children: [
+        {
+            path: 'markdown',
+            name: 'Markdown',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: 'Markdown编辑器'
+            }
+        },
+        {
+            path: 'rich-text',
+            name: 'ImageRichText',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '富文本编辑器'
+            }
+        }
+    ]
+}
