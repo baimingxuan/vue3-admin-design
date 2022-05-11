0b4728da000f7388df1c64a3d1029f9b13255118
baimingxuan <354372738@qq.com>
2022-05-11 20:38:25 +0800

    feat: add video route

---
+++ src/router/routes/modules/video.ts
@@ -0,0 +1,31 @@
+import type { AppRoute } from '../../types'
+import Layout from '@/layout/index.vue'
+
+export const VideoRoute: AppRoute = {
+    path: '/video',
+    name: 'Video',
+    component: Layout,
+    redirect: '/video/video-player',
+    meta: {
+        title: '视频处理',
+        icon: 'vue-dsn-icon-video'
+    },
+    children: [
+        {
+            path: 'video-player',
+            name: 'VideoPlayer',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '视频播放器'
+            }
+        },
+        {
+            path: 'video-mark',
+            name: 'VideoMark',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '视频水印'
+            }
+        }
+    ]
+}
