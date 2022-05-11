62e9d47fd8d9b6d49e8493d4d07f87e8e26ef410
baimingxuan <354372738@qq.com>
2022-05-11 20:45:10 +0800

    feat：add excel route

---
+++ src/router/routes/modules/excel.ts
@@ -0,0 +1,31 @@
+import type { AppRoute } from '../../types'
+import Layout from '@/layout/index.vue'
+
+export const ExcelRoute: AppRoute = {
+    path: '/excel',
+    name: 'Excel',
+    component: Layout,
+    redirect: '/excel/export-excel',
+    meta: {
+        title: 'Excel',
+        icon: 'vue-dsn-icon-excel'
+    },
+    children: [
+        {
+            path: 'export-excel',
+            name: 'ExportExcel',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '导出Excel'
+            }
+        },
+        {
+            path: 'import-excel',
+            name: 'ImportExcel',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '导入Excel'
+            }
+        }
+    ]
+}
