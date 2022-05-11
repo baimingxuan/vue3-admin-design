9fbb85cba560e64f220e820653e728e1f3116d84
baimingxuan <354372738@qq.com>
2022-05-11 20:37:56 +0800

    feat: add image route

---
+++ src/router/routes/modules/image.ts
@@ -0,0 +1,39 @@
+import type { AppRoute } from '../../types'
+import Layout from '@/layout/index.vue'
+
+export const ImageRoute: AppRoute = {
+    path: '/image',
+    name: 'Image',
+    component: Layout,
+    redirect: '/image/image-cropper',
+    meta: {
+        title: '图片处理',
+        icon: 'vue-dsn-icon-picture'
+    },
+    children: [
+        {
+            path: 'image-cropper',
+            name: 'ImageCropper',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '图片裁剪'
+            }
+        },
+        {
+            path: 'image-compress',
+            name: 'ImageCompress',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '图片压缩'
+            }
+        },
+        {
+            path: 'image-synthesizer',
+            name: 'ImageSynthesizer',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '图片合成'
+            }
+        }
+    ]
+}
