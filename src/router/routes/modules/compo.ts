bebcefce5b002eb1e9d5c128cbf19394a7e1e8f0
baimingxuan <354372738@qq.com>
2022-05-12 10:03:57 +0800

    feat: add compo route

---
+++ src/router/routes/modules/compo.ts
@@ -0,0 +1,66 @@
+import type { AppRoute } from '../../types'
+import Layout from '@/layout/index.vue'
+
+export const CompoRoute: AppRoute = {
+    path: '/compo',
+    name: 'Compo',
+    component: Layout,
+    redirect: '/compo/image-upload',
+    meta: {
+        title: '组件',
+        icon: 'vue-dsn-icon-zujian'
+    },
+    children: [
+        {
+            path: 'image-upload',
+            name: 'ImageUpload',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '图片上传'
+            }
+        },
+        {
+            path: 'drag',
+            name: 'Drag',
+            component: () => import('../../views/home.vue'),
+            redirect: '/tools/drag/drag-list',
+            meta: {
+                title: '拖拽'
+            },
+            children: [
+                {
+                    path: 'drag-list',
+                    name: 'DragList',
+                    component: () => import('../../views/home.vue'),
+                    meta: {
+                        title: '列表拖拽'
+                    }
+                },
+                {
+                    path: 'vue-drr',
+                    name: 'VueDrr',
+                    component: () => import('../../views/home.vue'),
+                    meta: {
+                        title: '组件拖拽'
+                    }
+                }
+            ]
+        },
+        {
+            path: 'transfer',
+            name: 'Transfer',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '穿梭框'
+            }
+        },
+        {
+            path: 'count-to',
+            name: 'CountTo',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '数字滚动'
+            }
+        }
+    ]
+}
