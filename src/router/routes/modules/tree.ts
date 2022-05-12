07c3ace32183ed1d6d86b94f82a7d895ef71dc77
baimingxuan <354372738@qq.com>
2022-05-12 10:03:36 +0800

    feat: add tree route

---
+++ src/router/routes/modules/tree.ts
@@ -0,0 +1,31 @@
+import type { AppRoute } from '../../types'
+import Layout from '@/layout/index.vue'
+
+export const TreeRoute: AppRoute = {
+    path: '/tree',
+    name: 'Tree',
+    component: Layout,
+    redirect: '/tree/org-tree',
+    meta: {
+        title: '树形结构',
+        icon: 'vue-dsn-icon-shuxing'
+    },
+    children: [
+        {
+            path: 'org-tree',
+            name: 'OrgTree',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '组织树'
+            }
+        },
+        {
+            path: 'ele-tree',
+            name: 'EleTree',
+            component: () => import('../../views/home.vue'),
+            meta: {
+                title: '控件树'
+            }
+        }
+    ]
+}
