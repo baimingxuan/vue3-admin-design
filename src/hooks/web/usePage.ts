472b5905163cbaea8504a574166ee55c2750fe42
baimingxuan <354372738@qq.com>
2022-05-23 10:39:35 +0800

    feat: update usePage.ts

--- src/hooks/web/usePage.ts
+++ src/hooks/web/usePage.ts
@@ -1,15 +1,24 @@
-import type { Router } from 'vue-router'
+import type { Router, RouteLocationRaw } from 'vue-router'
 import { useRouter } from 'vue-router'
+
 import { isString } from '@/utils/is'
+
+// handle error
+function handleError(e: Error) {
+    console.log(e)
+}
 
 // go page
 export function useGo(_router?: Router) {
-    let router
-    if (!_router) {
-        router = useRouter()
-    }
-    function go(opt: string, isReplace = false) {
+    const { push, replace } = _router || useRouter()
+    function go(opt: RouteLocationRaw, isReplace = false) {
         if (!opt) return
+        if (isString(opt)) {
+            isReplace ? replace(opt).catch(handleError) : push(opt).catch(handleError)
+        } else {
+            const _opt = opt as RouteLocationRaw;
+            isReplace ? replace(_opt).catch(handleError) : push(_opt).catch(handleError)
+        }
     }
     return go
 }
