8c2b76b57946af5077f8cbea30fd5d9e0114bfa9
baimingxuan <354372738@qq.com>
2022-05-24 16:44:17 +0800

    feat; update MenuItemContent

--- src/layout/menu/components/MenuItemContent.vue
+++ src/layout/menu/components/MenuItemContent.vue
@@ -1,11 +1,29 @@
 <template>
-  <span></span>
+  <span>
+    <SvgIcon v-if="getIcon" :name="getIcon" :size="18" />
+    {{ getIconName }}
+  </span>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, computed } from 'vue'
+  import { menuItemContentProps } from '../props'
+
+  import SvgIcon from '@/components/SvgIcon/index.vue'
 
   export default defineComponent({
-    name: 'MenuItemContent'
+    name: 'MenuItemContent',
+    components: { SvgIcon },
+    props: menuItemContentProps,
+
+    setup(props) {
+      const getIcon = computed(() => props.item?.icon)
+      const getIconName = computed(() => props.item?.name)
+
+      return {
+        getIcon,
+        getIconName
+      }
+    }
   })
 </script>
