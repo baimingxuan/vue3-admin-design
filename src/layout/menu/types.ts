d6eff19ad1a1a5cf40ceca5398852065169e650f
baimingxuan <354372738@qq.com>
2022-05-24 10:50:00 +0800

    feat: add menu types

---
+++ src/layout/menu/types.ts
@@ -0,0 +1,11 @@
+export interface MenuState {
+    // array with the keys of default selected menu items
+    defaultSelectedKeys: string[]
+
+    // array with the keys of currently opened sub menus
+    openKeys: string[]
+
+    // array with the keys of currently selected menu items
+    selectedKeys: string[]
+
+}
