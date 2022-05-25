ebff2e0b07e5c3651e7398c826ca37a64b2d4a89
baimingxuan <354372738@qq.com>
2022-05-25 10:54:51 +0800

    feat: update router/menus

--- src/router/menus/index.ts
+++ src/router/menus/index.ts
@@ -1,11 +1,35 @@
-import type { AppMenu } from '../types'
+import type { AppMenu, AppMenuModule } from '../types'
+import { transformMenuModule } from '../helper/menuHelper'
 
 const routeModules = import.meta.globEager('./modules/*.ts')
 
-const menuList: AppMenu[] = []
+const menuModules: AppMenuModule[] = []
 
 Object.keys(routeModules).forEach(key => {
     const module = routeModules[key].default || {}
     const moduleList = Array.isArray(module) ? [...module] : [module]
-    menuList.push(...moduleList)
+    menuModules.push(...moduleList)
 })
+
+
+export const getMenus = async (): Promise<AppMenu[]> => {
+
+    async function generateMenus() {
+        const staticMenus: AppMenu[] = []
+
+        menuModules.sort((a, b) => {
+            return (a.orderNo || 0) - (b.orderNo || 0)
+        })
+
+        for (const menu of menuModules) {
+            staticMenus.push(transformMenuModule(menu))
+        }
+
+        return staticMenus
+    }
+    
+    const menus = await generateMenus()
+
+    return menus
+}
+
