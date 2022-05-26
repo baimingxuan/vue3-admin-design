4a7013f87ac0d7dc4c698a95ff8cdbfc1ae78d33
baimingxuan <354372738@qq.com>
2022-05-26 09:45:41 +0800

    feat: update treeHelper

--- src/utils/helper/treeHelper.ts
+++ src/utils/helper/treeHelper.ts
@@ -1,7 +1,7 @@
 interface TreeHelperConfig {
-  id: string;
-  children: string;
-  pid: string;
+  id: string
+  children: string
+  pid: string
 }
 
 const DEFAULT_CONFIG: TreeHelperConfig = {
@@ -14,18 +14,17 @@ const getConfig = (config: Partial<TreeHelperConfig>) => Object.assign({}, DEFAU
 
 export function filter<T = any>(
   tree: T[],
-  func: (n: T) => boolean,
   config: Partial<TreeHelperConfig> = {},
 ): T[] {
-  config = getConfig(config);
-  const children = config.children as string;
+  config = getConfig(config)
+  const children = config.children as string
   function listFilter(list: T[]) {
     return list
       .map((node: any) => ({ ...node }))
       .filter((node) => {
-        node[children] = node[children] && listFilter(node[children]);
-        return func(node) || (node[children] && node[children].length);
+        node[children] = node[children] && listFilter(node[children])
+        return (node[children] && node[children].length)
       });
   }
-  return listFilter(tree);
+  return listFilter(tree)
 }
