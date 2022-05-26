3027128401f52752a77cedefbbd9aa8bff22ccfa
baimingxuan <354372738@qq.com>
2022-05-26 10:36:46 +0800

    feat: add treeMap to treeHelper

--- src/utils/helper/treeHelper.ts
+++ src/utils/helper/treeHelper.ts
@@ -28,3 +28,32 @@ export function filter<T = any>(
   }
   return listFilter(tree)
 }
+
+// Extract tree specified structure
+export function treeMap<T = any>(treeData: T[], opt: { children?: string; conversion: Fn }): T[] {
+  return treeData.map(item => treeMapEach(item, opt))
+}
+
+// Extract tree specified structure
+export function treeMapEach(
+  data: any,
+  { children = 'children', conversion }: { children?: string; conversion: Fn },
+) {
+  const haveChildren = Array.isArray(data[children]) && data[children].length > 0
+  const conversionData = conversion(data) || {}
+  if (haveChildren) {
+    return {
+      ...conversionData,
+      [children]: data[children].map((index: number) =>
+        treeMapEach(index, {
+          children,
+          conversion
+        })
+      )
+    };
+  } else {
+    return {
+      ...conversionData
+    };
+  }
+}
