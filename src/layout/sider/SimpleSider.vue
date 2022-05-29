0ec1d76bb1b4c78644c37a59300551cb65e0f271
baimingxuan <354372738@qq.com>
2022-05-29 22:38:56 +0800

    feat: add simpleSider compo

---
+++ src/layout/sider/SimpleSider.vue
@@ -0,0 +1,18 @@
+<template>
+  <AntdSider
+    ref="siderRef"
+    breakpoint="lg"
+    collapsible
+  >
+  </AntdSider>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Layout  } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'SimpleSider',
+    components: { AntdSider: Layout.Sider }
+  })
+</script>
