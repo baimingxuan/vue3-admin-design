e25ddcc8606b71dbc805ce5a0263ca271a6a59d0
baimingxuan <354372738@qq.com>
2022-05-31 22:21:21 +0800

    feat: add HeaderTrigger compo

---
+++ src/layout/trigger/HeaderTrigger.vue
@@ -0,0 +1,32 @@
+<template>
+  <span :class="[theme]" @click="toggleCollapsed">
+    <MenuUnfoldOutlined v-if="getCollapsed" />
+    <MenuFoldOutlined v-else />
+  </span>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref, unref } from 'vue'
+  import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons-vue'
+  import { propTypes } from '@/utils/propTypes'
+
+  export default defineComponent({
+    name: 'HeaderTrigger',
+    components: { MenuUnfoldOutlined, MenuFoldOutlined },
+    props: {
+      theme: propTypes.oneOf(['light', 'dark']),
+    },
+
+    setup() {
+      const getCollapsed = ref(false)
+
+      function toggleCollapsed() {
+        getCollapsed.value = !getCollapsed.value
+      }
+      return {
+        getCollapsed,
+        toggleCollapsed
+      }
+    }
+  })
+</script>
