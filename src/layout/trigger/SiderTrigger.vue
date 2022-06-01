ca032e7fc01e3760965a81c363918df80b541607
baimingxuan <354372738@qq.com>
2022-06-01 22:40:18 +0800

    feat: add SiderTrigger compo

---
+++ src/layout/trigger/SiderTrigger.vue
@@ -0,0 +1,29 @@
+<template>
+  <div @click.stop="toggleCollapsed">
+    <DoubleRightOutlined v-if="getCollapsed" />
+    <DoubleLeftOutlined v-else />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import { DoubleRightOutlined, DoubleLeftOutlined } from '@ant-design/icons-vue'
+
+  export default defineComponent({
+    name: 'SiderTrigger',
+    components: { DoubleRightOutlined, DoubleLeftOutlined },
+
+    setup() {
+      const getCollapsed = ref(false)
+
+      function toggleCollapsed() {
+        getCollapsed.value = !getCollapsed.value
+      }
+
+      return {
+        getCollapsed,
+        toggleCollapsed
+      }
+    }
+  })
+</script>
