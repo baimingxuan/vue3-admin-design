9529c53599618580b46cab8503543aeb1dcf08df
baimingxuan <354372738@qq.com>
2022-06-02 22:58:28 +0800

    feat: add trigger component

---
+++ src/layout/trigger/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <SiderTrigger v-if="sider" />
+  <HeaderTrigger v-else :theme="theme" />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { ImportAsyncComponent } from '@/utils/factory/ImportAsyncComponent'
+  import { propTypes } from '@/utils/propTypes'
+  import HeaderTrigger from './HeaderTrigger.vue'
+
+  export default defineComponent({
+    name: 'LayoutTrigger',
+    components: {
+      SiderTrigger: ImportAsyncComponent(() => import('./SiderTrigger.vue')),
+      HeaderTrigger
+    },
+    props: {
+      sider: propTypes.bool.def(true),
+      theme: propTypes.oneOf(['light', 'dark'])
+    }
+  })
+
+</script>
