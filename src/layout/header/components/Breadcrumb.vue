7bb08c48bf4f1bf3770be2fdb0418d8760a188df
baimingxuan <354372738@qq.com>
2022-06-03 23:54:38 +0800

    feat: add BreadCrumb compo

---
+++ src/layout/header/components/Breadcrumb.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <AntdBreadcrumb></AntdBreadcrumb>
+  </div>
+</template>
+
+<script lang="ts">
+  import type { RouteLocationMatched } from 'vue-router'
+  import { useRouter } from 'vue-router'
+  import type { AppMenu } from '@/router/types'
+
+  import { defineComponent, ref } from 'vue'
+  import { Breadcrumb as AntdBreadcrumb } from 'ant-design-vue'
+</script>
