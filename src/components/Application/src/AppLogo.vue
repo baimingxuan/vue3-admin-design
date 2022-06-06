107a0f5d0b070231845c5e38f27491ab321db634
baimingxuan <354372738@qq.com>
2022-06-06 21:11:17 +0800

    feta: add AppLogo compo

---
+++ src/components/Application/src/AppLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div @click="goHome">
+    <img src="../../../assets/images/logo.png" alt="logo">
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useGo } from '@/hooks/web/usePage'
+
+  const go = useGo()
+
+  function goHome() {
+    go('/')
+  }
+</script>
