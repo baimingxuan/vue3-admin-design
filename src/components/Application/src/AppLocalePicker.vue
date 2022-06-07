e6378c72ef0e10997ffcc95eb25e515df6c5a9fc
baimingxuan <354372738@qq.com>
2022-06-07 23:11:03 +0800

    feat: add AppLocalePicker compo

---
+++ src/components/Application/src/AppLocalePicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <AntdDropdown
+    placement="bottom"
+  >
+    <span>切换语言</span>
+    <template #overlay>
+      <AntdMenu>
+        <AntdMenuItem>简体中文</AntdMenuItem>
+        <AntdMenuItem>繁體中文</AntdMenuItem>
+        <AntdMenuItem>English</AntdMenuItem>
+      </AntdMenu>
+    </template>
+  </AntdDropdown>
+</template>
+
+<script lang="ts" setup>
+  import { Dropdown as AntdDropdown, Menu as AntdMenu, MenuItem as AntdMenuItem } from 'ant-design-vue'
+</script>
