fc8121a3a148f996c0b6e478cf6075ff6dd49833
baimingxuan <354372738@qq.com>
2022-06-08 23:06:36 +0800

    feat: update utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import type { App, Plugin } from 'vue'
+import { isObject } from './is'
 
 export const withInstall = <T>(component: T, alias?: string) => {
   const compo = component as any
@@ -9,4 +10,12 @@ export const withInstall = <T>(component: T, alias?: string) => {
     }
   }
   return component as T & Plugin
+}
+
+export function deepMerge<T = any>(src: any = {}, target: any = {}): T {
+  let key: string
+  for (key in target) {
+    src[key] = isObject(src[key]) ? deepMerge(src[key], target[key]) : (src[key] = target[key])
+  }
+  return src
 }
