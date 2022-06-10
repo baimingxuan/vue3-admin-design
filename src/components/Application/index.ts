cb5471836206569778828f686d27e9063dcfe777
baimingxuan <354372738@qq.com>
2022-06-10 23:24:30 +0800

    feat: update application/index.ts

--- src/components/Application/index.ts
+++ src/components/Application/index.ts
@@ -1,5 +1,7 @@
 import { withInstall } from '@/utils'
 
 import appLogo from './src/AppLogo.vue'
+import appLocalePicker from './src/AppLocalePicker.vue'
 
 export const AppLogo = withInstall(appLogo)
+export const AppLocalePicker = withInstall(appLocalePicker)
