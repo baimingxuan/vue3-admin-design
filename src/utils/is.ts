16c13797f83c33dabc9225bc3c5bf6940fe28134
baimingxuan <354372738@qq.com>
2022-06-12 23:00:54 +0800

    feat: update utils/is.ts

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -30,6 +30,14 @@ export function isUnDef<T = unknown>(val?: T): val is T {
     return !isDef(val)
 }
 
+export function isNull(val: unknown): val is null {
+    return val === null
+  }
+
+export function isNullOrUnDef(val: unknown): val is null | undefined {
+    return isUnDef(val) || isNull(val)
+}
+
 export function isUrl(path: string): boolean {
     const reg =
       /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
