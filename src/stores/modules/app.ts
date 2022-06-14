f240c6a2a09f9e4b876b6f4949a804421afcb6f3
baimingxuan <354372738@qq.com>
2022-06-14 21:02:22 +0800

    feat: update stores/app

--- src/stores/modules/app.ts
+++ src/stores/modules/app.ts
@@ -6,6 +6,7 @@ import { ThemeMode } from '@/types'
 import { ThemeEnum } from '@/enums/appEnum'
 import { deepMerge } from '@/utils'
 import { Persistent } from '@/utils/cache/persistent'
+import { APP_CONFIG_KEY } from '@/enums/cacheEnum'
 
 interface AppState {
   themeMode?: ThemeEnum
@@ -16,7 +17,7 @@ interface AppState {
 export const useAppStore = defineStore('app', {
   state: (): AppState => ({
     themeMode: undefined,
-    appConfig: Persistent.getLocal('APP_CONFIG_KEY')
+    appConfig: Persistent.getLocal(APP_CONFIG_KEY)
   }),
 
   getters: {
@@ -37,7 +38,7 @@ export const useAppStore = defineStore('app', {
     },
     setAppConfig(config: DeepPartial<AppConfig>): void {
       this.appConfig = deepMerge(this.appConfig || {}, config)
-      Persistent.setLocal('APP_CONFIG_KEY', this.appConfig)
+      Persistent.setLocal(APP_CONFIG_KEY, this.appConfig)
     }
   }
 })
