c7244d41041046f0d518065b878b4e91976cd1db
baimingxuan <354372738@qq.com>
2022-06-29 19:55:51 +0800

    feat: update cache/index.ts

---
+++ src/utils/cache/index.ts
@@ -0,0 +1,33 @@
+import { isDevMode } from '../env'
+import { createStorage as create, CreateStorageParams } from './storageCache'
+
+type Options = Partial<CreateStorageParams>
+
+const enableStorageEncrypt = !isDevMode()
+const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 7
+
+const createOptions = (storage: Storage, options: Options = {}): Options => {
+    return {
+      // No encryption in debug mode
+      hasEncrypt: enableStorageEncrypt,
+      storage,
+      prefixKey: 'vue-admin-design',
+      ...options
+    }
+}
+
+const WebStorage = create(createOptions(sessionStorage))
+
+export const createStorage = (storage: Storage = sessionStorage, options: Options = {}) => {
+    return create(createOptions(storage, options))
+};
+  
+export const createSessionStorage = (options: Options = {}) => {
+    return createStorage(sessionStorage, { ...options, timeout: DEFAULT_CACHE_TIME })
+}
+
+export const createLocalStorage = (options: Options = {}) => {
+    return createStorage(localStorage, { ...options, timeout: DEFAULT_CACHE_TIME })
+};
+
+export default WebStorage
