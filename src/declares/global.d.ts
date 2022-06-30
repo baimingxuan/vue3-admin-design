924052f29a81c07d92176ae289ffe4051f333340
baimingxuan <354372738@qq.com>
2022-06-30 19:17:05 +0800

    feat: add declares to global.d.ts

--- src/declares/global.d.ts
+++ src/declares/global.d.ts
@@ -2,6 +2,10 @@ declare type Nullable<T> = T | null
 declare type Recordable<T = any> = Record<string, T>
 declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>
 
+declare type DeepPartial<T> = {
+    [P in keyof T]?: DeepPartial<T[P]>
+}
+
 declare interface ViteEnv {
     VITE_PORT: number;
     VITE_PUBLIC_PATH: string;
