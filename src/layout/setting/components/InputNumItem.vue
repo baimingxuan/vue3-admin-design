6e09c72f45ff5bc3e51d249377537ac2f90097e0
baimingxuan <354372738@qq.com>
2022-07-13 20:10:16 +0800

    feat: update InputNumItem compo

--- src/layout/setting/components/InputNumItem.vue
+++ src/layout/setting/components/InputNumItem.vue
@@ -2,16 +2,21 @@
   <div class="compo_input-num-item">
     <span>{{ title }}</span>
     <AntdInputNum
+      class="input-number"
       size="small"
       v-bind="$attrs"
+      @change="handleChange"
     />
   </div>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
+  import type { PropType } from 'vue'
 
   import { InputNumber as AntdInputNum } from 'ant-design-vue'
+  import { HandlerEnum } from '../enum'
+  import { baseHandler } from '../handler'
 
   export default defineComponent({
     name: 'InputNumItem',
@@ -20,9 +25,20 @@
       title: {
         type: String,
         default: ''
+      },
+      event: {
+        type: Number as PropType<HandlerEnum>
       }
     },
-    setup(props) {}
+    setup(props) {
+      function handleChange(value: number | string) {
+        props.event && baseHandler(props.event, value)
+      }
+
+      return {
+        handleChange
+      }
+    }
   })
 </script>
 
@@ -32,7 +48,7 @@
     justify-content: space-between;
     margin: 16px 0;
 
-    &-input-numbel {
+    .input-number {
       width: 120px;
     }
   }
