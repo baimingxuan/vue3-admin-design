004cc9162b1a316b2951b5343dcd2bb7dad058d3
baimingxuan <354372738@qq.com>
2022-07-15 21:50:27 +0800

    feat: update PageTransitionEnum

--- src/enums/appEnum.ts
+++ src/enums/appEnum.ts
@@ -7,6 +7,9 @@ export enum ThemeEnum {
 // page switching animation
 export enum PageTransitionEnum {
   FADE = 'fade',
+  FADE_SIDE = 'fade-slide',
+  FADE_BOTTOM = 'fade-bottom',
   FADE_SCALE = 'fade-scale',
-  ZOOM_FADE = 'zoom-fade'
+  ZOOM_FADE = 'zoom-fade',
+  ZOOM_OUT = 'zoom-out'
 }
