7b80b5e6acc2bfc611d7ba6afe1c077fe3007325
baimingxuan <354372738@qq.com>
2022-07-21 21:56:56 +0800

    feat: update Page compo

--- src/layout/content/components/Page.vue
+++ src/layout/content/components/Page.vue
@@ -1,19 +1,32 @@
 <template>
   <router-view v-slot="{ Component, route }">
     <transition :name="'fade'" mode="out-in" appear>
-      <keep-alive>
+      <keep-alive v-if="openPageCache">
         <component :is="Component" :key="route.fullPath" />
       </keep-alive>
+      <component v-else :is="Component" :key="route.fullPath" />
     </transition>
   </router-view>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, computed, unref } from 'vue'
+
+  import { useBaseSetting } from '@/hooks/setting/useBaseSetting'
+  import { useHeaderSetting } from '@/hooks/setting/useHeaderSetting'
 
   export default defineComponent({
     name: 'LayoutPage',
 
-    setup() {}
+    setup() {
+      const { getOpenKeepAlive } = useBaseSetting()
+      const { getShowPageTags } = useHeaderSetting()
+      const openPageCache = computed(() => unref(getOpenKeepAlive) && unref(getShowPageTags))
+
+      return {
+        openPageCache
+      }
+
+    }
   })
 </script>
