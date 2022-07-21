22613fdc7039e9651fdd6f8a2031523145ceb8b4
baimingxuan <354372738@qq.com>
2022-07-21 19:02:28 +0800

    feat: add Page compo to layout

---
+++ src/layout/content/components/Page.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view v-slot="{ Component, route }">
+    <transition :name="'fade'" mode="out-in" appear>
+      <keep-alive>
+        <component :is="Component" :key="route.fullPath" />
+      </keep-alive>
+    </transition>
+  </router-view>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'LayoutPage',
+
+    setup() {}
+  })
+</script>
