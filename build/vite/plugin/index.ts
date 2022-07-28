f5e115d2ff280c5b2130452a24a7bb46e8cd12b8
baimingxuan <354372738@qq.com>
2022-07-28 11:17:08 +0800

    chore: remove unplugin-vue-components

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import windiCSS from 'vite-plugin-windicss'
 import { configSvgIconsPlugin } from './svgIcons'
-import Components from 'unplugin-vue-components/vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export function createVitePlugins(isBuild: boolean) {
     const vitePlugins: (Plugin | Plugin[])[] = [
@@ -18,9 +16,6 @@ export function createVitePlugins(isBuild: boolean) {
 
     // vite-plugin-svg-icons
     vitePlugins.push(configSvgIconsPlugin(isBuild))
-
-    // unplugin-vue-components -- on-demand components auto importing for vue
-    vitePlugins.push(Components({resolvers: [AntDesignVueResolver()]}))
 
     return vitePlugins
 }
