2f9c593ab5328c8417a49a754fce5299bc228e9c
baimingxuan <354372738@qq.com>
2022-07-29 16:18:49 +0800

    feat: update global.d.ts

--- src/declares/global.d.ts
+++ src/declares/global.d.ts
@@ -2,6 +2,9 @@ declare type Nullable<T> = T | null
 declare type Recordable<T = any> = Record<string, T>
 declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>
 
+declare type TimeoutHandle = ReturnType<typeof setTimeout>
+declare type IntervalHandle = ReturnType<typeof setInterval>
+
 declare type DeepPartial<T> = {
     [P in keyof T]?: DeepPartial<T[P]>
 }
