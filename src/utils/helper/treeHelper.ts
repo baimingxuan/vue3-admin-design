b8546fd3a427e039460b337ada32067e1e029503
baimingxuan <354372738@qq.com>
2022-07-29 16:19:44 +0800

    feat: add findPath func to treeHelper.ts

--- src/utils/helper/treeHelper.ts
+++ src/utils/helper/treeHelper.ts
@@ -12,6 +12,33 @@ const DEFAULT_CONFIG: TreeHelperConfig = {
 
 const getConfig = (config: Partial<TreeHelperConfig>) => Object.assign({}, DEFAULT_CONFIG, config)
 
+export function findPath<T = any>(
+  tree: any,
+  func: Fn,
+  config: Partial<TreeHelperConfig> = {},
+): T | T[] | null {
+  config = getConfig(config)
+  const path: T[] = []
+  const list = [...tree]
+  const visitedSet = new Set()
+  const { children } = config
+  while (list.length) {
+    const node = list[0]
+    if (visitedSet.has(node)) {
+      path.pop()
+      list.shift()
+    } else {
+      visitedSet.add(node)
+      node[children!] && list.unshift(...node[children!])
+      path.push(node)
+      if (func(node)) {
+        return path
+      }
+    }
+  }
+  return null
+}
+
 export function filter<T = any>(
   tree: T[],
   config: Partial<TreeHelperConfig> = {},
