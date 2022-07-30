15a433763bba49ae47c082f9cdf7e7623f59fea2
baimingxuan <354372738@qq.com>
2022-07-30 22:16:41 +0800

    feat: update router/menu

--- src/router/menus/index.ts
+++ src/router/menus/index.ts
@@ -1,44 +1,46 @@
-import type { AppMenu, AppMenuModule } from '../types'
-import { transformMenuModule } from '../helper/menuHelper'
-import { usePermissionStore } from '@/stores/modules/permission'
-import { getAllParentPath } from '@/router/helper/menuHelper'
+import type { AppMenu, AppMenuModule } from "../types";
+import { transformMenuModule } from "../helper/menuHelper";
+import { usePermissionStore } from "@/stores/modules/permission";
+import { getAllParentPath } from "@/router/helper/menuHelper";
 
-const routeModules = import.meta.globEager('./routes/modules/*.ts')
+const routeModules = import.meta.glob("./routes/modules/*.ts", { eager: true });
 
-const menuModules: AppMenuModule[] = []
+const menuModules: AppMenuModule[] = [];
 
-Object.keys(routeModules).forEach(key => {
-    const module = routeModules[key].default || {}
-    const moduleList = Array.isArray(module) ? [...module] : [module]
-    menuModules.push(...moduleList)
-})
+Object.keys(routeModules).forEach((key) => {
+  const module = routeModules[key].default || {};
+  const moduleList = Array.isArray(module) ? [...module] : [module];
+  menuModules.push(...moduleList);
+});
+
+async function getAsyncMenus() {
+  
+}
 
 export async function getCurrentParentPath(currentPath: string) {
-    const menus = await getAsyncMenus()
-    const allParentPath = await getAllParentPath(menus, currentPath)
-    return allParentPath?.[0]
+  const menus = await getAsyncMenus();
+  const allParentPath = await getAllParentPath(menus, currentPath);
+  return allParentPath?.[0];
 }
 
 export const getMenus = async (): Promise<AppMenu[]> => {
+  async function generateMenus() {
+    const permissionStore = usePermissionStore();
+    // const staticMenus: AppMenu[] = []
 
-    async function generateMenus() {
-        const permissionStore = usePermissionStore()
-        // const staticMenus: AppMenu[] = []
+    // menuModules.sort((a, b) => {
+    //     return (a.orderNo || 0) - (b.orderNo || 0)
+    // })
 
-        // menuModules.sort((a, b) => {
-        //     return (a.orderNo || 0) - (b.orderNo || 0)
-        // })
+    // for (const menu of menuModules) {
+    //     staticMenus.push(transformMenuModule(menu))
+    // }
 
-        // for (const menu of menuModules) {
-        //     staticMenus.push(transformMenuModule(menu))
-        // }
+    // return staticMenus
+    return permissionStore.getMenuList.filter((item) => !item.hideMenu);
+  }
 
-        // return staticMenus
-        return permissionStore.getMenuList.filter(item => !item.hideMenu)
-    }
-    
-    const menus = await generateMenus()
+  const menus = await generateMenus();
 
-    return menus
-}
-
+  return menus;
+};
