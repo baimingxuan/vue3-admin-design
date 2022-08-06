a80fe0d0a61d1d296ec0443dfa4a6c36ca4dc00c
baimingxuan <354372738@qq.com>
2022-08-06 22:31:04 +0800

    feat: update permissionGuard.ts

--- src/router/guard/permissionGuard.ts
+++ src/router/guard/permissionGuard.ts
@@ -2,9 +2,19 @@ import type { Router, RouteRecordRaw } from 'vue-router'
 import { usePermissionStoreWithOut } from '@/stores/modules/permission'
 
 export function createPermissionGuard(router: Router) {
-    const permissionStore = usePermissionStoreWithOut()
-    router.beforeEach(async (to, from, next) => {
-        const routes = await permissionStore.buildRoutesAction()
-        next()
+  const permissionStore = usePermissionStoreWithOut()
+
+  router.beforeEach(async (to, from, next) => {
+    if (from.path === '/' && to.path === '/home') {
+      next('/home')
+      return
+    }
+
+    const routes = await permissionStore.buildRoutesAction()
+
+    routes.forEach(route => {
+        router.addRoute(route as RouteRecordRaw)
     })
+    next('/')
+  })
 }
