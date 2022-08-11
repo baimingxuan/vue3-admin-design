dd73be3625faec7691056b64b7ced4c0f53f42f6
baimingxuan <354372738@qq.com>
2022-08-11 16:57:19 +0800

    feat: update BasicSider compo

--- src/layout/sider/BasicSider.vue
+++ src/layout/sider/BasicSider.vue
@@ -3,16 +3,38 @@
     ref="siderRef"
     breakpoint="lg"
     collapsible
+    :theme="getMenuTheme"
+    :width="getMenuWidth"
+    :collapsed="getMenuFold"
   >
+    <template #trigger>
+      <LayoutTrigger />
+    </template>
+    <LayoutMenu />
+    <DragBar ref="dragBarRef" />
   </AntdSider>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
-  import { Layout  } from 'ant-design-vue'
+  import { Layout } from 'ant-design-vue'
+  import LayoutTrigger from '@/layout/trigger/index.vue'
+  import DragBar from './components/DragBar.vue'
+  import LayoutMenu from '@/layout/menu/index.vue'
+  import { useMenuSetting } from '@/hooks/setting/useMenuSetting'
 
   export default defineComponent({
     name: 'SimpleSider',
-    components: { AntdSider: Layout.Sider }
+    components: { AntdSider: Layout.Sider, LayoutTrigger, DragBar, LayoutMenu },
+
+    setup() {
+      const { getMenuTheme, getMenuWidth, getMenuFold } = useMenuSetting()
+
+      return {
+        getMenuTheme,
+        getMenuWidth,
+        getMenuFold
+      }
+    }
   })
 </script>
