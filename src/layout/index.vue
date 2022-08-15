9749d5e5d58628c17bd2b9ec41323c4572cf83f3
baimingxuan <354372738@qq.com>
2022-08-15 16:42:13 +0800

    feat: update layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,23 +1,32 @@
 <template>
-  <AntLayout>
-    <!-- <BasicSider /> -->
-    <HybridSider />
+  <AntLayout :class="prefixCls">
+    <BasicSider />
     <AntLayout>
-      <AntHeader>Header</AntHeader>
+      <!-- <HybridSider /> -->
+      <LayoutHeader />
       <AntContent>
         <LayoutPage />
       </AntContent>
+      <!-- <AntLayout>
+        <LayoutHeader />
+        <AntContent>
+          <LayoutPage />
+        </AntContent>
+      </AntLayout> -->
     </AntLayout>
     <Setting></Setting>
   </AntLayout>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, computed, unref } from 'vue'
   import { Layout as AntLayout } from 'ant-design-vue'
+
+  import { useMenuSetting } from '@/hooks/setting/useMenuSetting'
   import Setting from './setting/index.vue'
-  // import BasicSider from './sider/BasicSider.vue'
-  import HybridSider from './sider/HybridSider/index.vue'
+  import BasicSider from './sider/BasicSider.vue'
+  // import HybridSider from './sider/HybridSider/index.vue'
+  import LayoutHeader from './header/index.vue'
   import LayoutPage from './content/components/Page.vue'
 
   export default defineComponent({
@@ -25,12 +34,41 @@
     components: {
       AntLayout,
       AntSider: AntLayout.Sider,
-      AntHeader: AntLayout.Header,
+      LayoutHeader,
       AntContent: AntLayout.Content,
       Setting,
-      // BasicSider,
-      HybridSider,
+      BasicSider,
+      // HybridSider,
       LayoutPage
+    },
+
+    setup() {
+      const prefixCls = 'layout-container'
+
+      const { getIsHybridMenu } = useMenuSetting()
+
+      const getLayoutCls = computed(() => {
+        let cls: string[] = ['ant-layout']
+        if (unref(getIsHybridMenu)) {
+          cls.push('ant-layout-has-sider')
+        }
+        return cls
+      })
+
+      return {
+        prefixCls,
+        getLayoutCls
+      }
     }
   })
 </script>
+
+<style lang="less" scoped>
+  @prefix-cls: ~'layout-container';
+
+  .@{prefix-cls} {
+    display: flex;
+    width: 100%;
+    min-height: 100%;
+  }
+</style>
