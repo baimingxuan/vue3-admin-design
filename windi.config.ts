ec4c5c430b011d7135eab011a52596c73a00ff39
baimingxuan <354372738@qq.com>
2022-08-16 15:24:43 +0800

    feat: add windicss plugin

--- windi.config.ts
+++ windi.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
                 'md': '768px',
                 'lg': '992px',
                 'xl': '1200px',
-                '2xl': '1600px',
+                '2xl': '1600px'
             }
         }
     }
