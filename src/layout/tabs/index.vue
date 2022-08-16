bea94b4562d69d42757b11530bbdb34dc2246e89
baimingxuan <354372738@qq.com>
2022-08-16 23:23:52 +0800

    feat: add LayoutTabs compo

---
+++ src/layout/tabs/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout_tabs"></div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Tabs as AntdTabs } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'LayoutTabs',
+    components: { AntdTabs },
+
+    setup() {
+    }
+  })
+</script>
+
+<style lang="less" scoped>
+
+</style>
