cb357897dea90947c81db94d6028999d461e0b2d
baimingxuan <354372738@qq.com>
2022-08-17 14:46:08 +0800

    feat: developing Tags compo

--- src/layout/tags/index.vue
+++ src/layout/tags/index.vue
@@ -1,20 +1,51 @@
 <template>
-  <div :class="prefixCls"></div>
+  <div :class="prefixCls">
+    <AntdButton :class="`${prefixCls}__btn`" @click="handleMove(240)">
+      <template #icon>
+        <LeftOutlined />
+      </template>
+    </AntdButton>
+    <div :class="`${prefixCls}__main`">
+      <div :class="`${prefixCls}__main-body`">
+        <TransitionGroup>
+          <template v-for="tag in getTabs" :key="tag">
+            <TagItem />
+          </template>
+        </TransitionGroup>
+      </div>
+    </div>
+    <AntdButton :class="`${prefixCls}__btn`" @click="handleMove(-240)">
+      <template #icon>
+        <RightOutlined />
+      </template>
+    </AntdButton>
+  </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import { Tabs as AntdTabs } from 'ant-design-vue'
+  import { defineComponent, ref } from 'vue'
+  import { Tabs as AntdTabs, Button as AntdButton } from 'ant-design-vue'
+  import { LeftOutlined, RightOutlined } from '@ant-design/icons-vue'
+
+  import TagItem from './components/TagItem.vue'
 
   export default defineComponent({
     name: 'LayoutTabs',
-    components: { AntdTabs },
+    components: { AntdTabs, AntdButton, TagItem, LeftOutlined, RightOutlined },
 
     setup() {
       const prefixCls = 'layout_tabs'
 
+      const getTabs = ref([])
+
+      function handleMove(offset: number): void {
+
+      }
+
       return {
-        prefixCls
+        prefixCls,
+        getTabs,
+        handleMove
       }
     }
   })
