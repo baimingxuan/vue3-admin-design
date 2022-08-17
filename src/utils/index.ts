99fc4afd56ac05f52884fd467db8b2d25d0c9956
baimingxuan <354372738@qq.com>
2022-08-17 20:18:07 +0800

    feat: add getRawRoute func to untils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import type { App, Plugin } from 'vue'
+import type { RouteLocationNormalized, RouteRecordNormalized } from 'vue-router'
 import { isObject } from './is'
 
 export const withInstall = <T>(component: T, alias?: string) => {
@@ -18,4 +19,21 @@ export function deepMerge<T = any>(src: any = {}, target: any = {}): T {
     src[key] = isObject(src[key]) ? deepMerge(src[key], target[key]) : (src[key] = target[key])
   }
   return src
+}
+
+export function getRawRoute(route: RouteLocationNormalized): RouteLocationNormalized {
+  if (!route) return route
+
+  const { matched, ...opt } = route
+
+  return {
+    ...opt,
+    matched: (matched
+      ? matched.map((item) => ({
+          meta: item.meta,
+          name: item.name,
+          path: item.path
+        }))
+      : undefined) as RouteRecordNormalized[]
+  }
 }
