1acf76b529869918aa089d5da33060ea86d7657f
baimingxuan <354372738@qq.com>
2022-08-21 22:52:48 +0800

    feat: update MultipleHeader compo

--- src/layout/header/MultipleHeader.vue
+++ src/layout/header/MultipleHeader.vue
@@ -1,6 +1,17 @@
 <template>
-  <div></div>
+  <AntdHeader>
+    <LayoutTags />
+  </AntdHeader>
 </template>
 
 <script lang="ts">
+  import { defineComponent } from 'vue'
+  import LayoutTags from '../tags/index.vue'
+  import { Layout } from 'ant-design-vue'
+
+  export default defineComponent({
+    name: 'LayoutMultipleHeader',
+    components: { LayoutTags, AntdHeader: Layout.Header }
+  })
+
 </script>
