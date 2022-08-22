9228eeaaf8ec282a7d7c8b527105a8504a2debd4
baimingxuan <354372738@qq.com>
2022-08-22 09:45:46 +0800

    feat: update router/guard.ts

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -1,6 +1,26 @@
 import type { Router } from 'vue-router'
 import { createPermissionGuard } from './permissionGuard'
+import { setRouteChange } from '@/logics/mitt/routeChange'
 
 export function setupRouterGuard(router: Router) {
-    createPermissionGuard(router)
+  createPageGuard(router)
+  createPermissionGuard(router)
+}
+
+// Hooks for handling page state
+function createPageGuard(router: Router) {
+  const loadedPageMap = new Map<string, boolean>()
+
+  router.beforeEach(async (to) => {
+    // The page has already been loaded, it will be faster to open it again, you don’t need to do loading and other processing
+    to.meta.loaded = !!loadedPageMap.get(to.path)
+    // Notify routing changes
+    setRouteChange(to)
+
+    return true
+  })
+
+  router.afterEach((to) => {
+    loadedPageMap.set(to.path, true)
+  })
 }
