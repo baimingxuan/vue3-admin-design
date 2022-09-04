3c1f84f6783d740542e38d9c3b85e54bd2a1350d
baimingxuan <354372738@qq.com>
2022-09-04 23:06:49 +0800

    feat：MultipleHeader renamed SimpleHeader

---
+++ src/layout/header/SimpleHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <AntdHeader style="background: #fff">
+    <div class="">
+      <LayoutTrigger :sider="false" />
+    </div>
+    <LayoutTags />
+  </AntdHeader>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Layout } from 'ant-design-vue'
+
+  import LayoutTrigger from '@/layout/trigger/index.vue'
+  import LayoutTags from '../tags/index.vue'
+  
+
+  export default defineComponent({
+    name: 'LayoutSimpleHeader',
+    components: { AntdHeader: Layout.Header, LayoutTrigger, LayoutTags, }
+  })
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
