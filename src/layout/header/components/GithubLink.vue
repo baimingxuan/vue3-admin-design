41bb42b0d3a87d8e5e453ece5183f6cbc5b5522e
baimingxuan <354372738@qq.com>
2022-09-05 22:23:21 +0800

    feat: Add GithubLink component

---
+++ src/layout/header/components/GithubLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <AntdTooltip
+    title="github"
+    placement='bottom'
+    :mouseEnterDelay="0.5"
+  >
+    <SvgIcon
+      name="github"
+      :size="20"
+    />
+  </AntdTooltip>
+</template>
+
+<script lang="ts" setup>
+  import { Tooltip as AntdTooltip } from 'ant-design-vue'
+  import SvgIcon from '@/components/SvgIcon/index.vue'
+</script>
