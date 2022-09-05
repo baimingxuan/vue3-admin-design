3dff868df81dabd1bd2ce73ba807257944294540
baimingxuan <354372738@qq.com>
2022-09-05 21:56:02 +0800

    feat: add LocalePicker component

---
+++ src/layout/header/components/LocalePicker.vue
@@ -0,0 +1,20 @@
+<template>
+  <AntdDropdown
+    placement="bottom"
+  >
+    <SvgIcon name="locale" :size="20" />
+    <span>切换语言</span>
+    <template #overlay>
+      <AntdMenu>
+        <AntdMenuItem>简体中文</AntdMenuItem>
+        <AntdMenuItem>繁體中文</AntdMenuItem>
+        <AntdMenuItem>English</AntdMenuItem>
+      </AntdMenu>
+    </template>
+  </AntdDropdown>
+</template>
+
+<script lang="ts" setup>
+  import { Dropdown as AntdDropdown, Menu as AntdMenu, MenuItem as AntdMenuItem } from 'ant-design-vue'
+  import SvgIcon from '@/components/SvgIcon/index.vue'
+</script>
