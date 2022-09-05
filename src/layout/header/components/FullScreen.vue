9ced7048771c6002daa0aaf107c5117dc8691d7a
baimingxuan <354372738@qq.com>
2022-09-05 21:10:07 +0800

    feat: Refactoring FullScreen component

---
+++ src/layout/header/components/FullScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <AntdTooltip :title="getTitle" placement='bottom' :mouseEnterDelay="0.5">
+    <span @click="toggle">
+      <ExpandOutlined v-if="!isFullscreen" />
+      <CompressOutlined v-else />
+    </span>
+  </AntdTooltip>
+</template>
+
+<script lang="ts" setup>
+  import { computed, unref } from 'vue'
+  import { Tooltip as AntdTooltip } from 'ant-design-vue'
+  import { useFullscreen } from '@vueuse/core'
+  import { ExpandOutlined, CompressOutlined } from '@ant-design/icons-vue'
+
+  const { isFullscreen, toggle } = useFullscreen()
+
+  const getTitle = computed(() => {
+    return unref(isFullscreen) ? '退出全屏' : '全屏'
+  })
+</script>
