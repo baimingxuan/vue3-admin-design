a57ab37775279688c952aa164f95fb50749edc00
baimingxuan <354372738@qq.com>
2022-09-06 16:02:30 +0800

    feat: Add LayoutFeature module

---
+++ src/layout/feature/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layout_feature">
+    <Search />
+    <FullScreen />
+    <LocalePicker />
+    <DocLink />
+    <GithubLink />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import {
+    Search,
+    FullScreen,
+    LocalePicker,
+    DocLink,
+    GithubLink
+  } from './components'
+</script>
+
+<style lang="less" scoped>
+  .layout_feature {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    min-width: 160px;
+  }
+</style>
