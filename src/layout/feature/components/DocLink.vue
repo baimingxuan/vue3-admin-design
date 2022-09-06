2c400493cfdec2de98786ed1fb209a53027a62ed
baimingxuan <354372738@qq.com>
2022-09-06 15:45:18 +0800

    feat: Add DocLink component

---
+++ src/layout/feature/components/DocLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <AntdTooltip
+    title="文档"
+    placement='bottom'
+    :mouseEnterDelay="0.5"
+  >
+    <span>
+      <SvgIcon
+        name="document"
+        :size="20"
+      />
+    </span>
+  </AntdTooltip>
+</template>
+
+<script lang="ts" setup>
+  import { Tooltip as AntdTooltip } from 'ant-design-vue'
+  import SvgIcon from '@/components/SvgIcon/index.vue'
+</script>
