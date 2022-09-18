caf80b0e65fb61bd352ddada18ce3af2c16011ec
baimingxuan <354372738@qq.com>
2022-09-18 22:30:43 +0800

    pref: update AppRoute interface

--- src/router/types.ts
+++ src/router/types.ts
@@ -2,31 +2,33 @@ import type { RouteRecordRaw, RouteMeta } from 'vue-router'
 import { defineComponent } from 'vue'
 
 export type Component<T = any> =
-    | ReturnType<typeof defineComponent>
-    | (() => Promise<typeof import('*.vue')>)
-    | (() => Promise<T>)
+  | ReturnType<typeof defineComponent>
+  | (() => Promise<typeof import('*.vue')>)
+  | (() => Promise<T>)
 
 // RouteRecordRaw detail, see: https://router.vuejs.org/api/#routerecordraw
 export interface AppRoute extends Omit<RouteRecordRaw, 'children'> {
-    name: string
-    meta: RouteMeta
-    component?: Component | string
-    children?: AppRoute[]
-    fullPath?: string
+  name: string
+  meta: RouteMeta
+  component?: Component | string
+  children?: AppRoute[]
+  fullPath?: string
+  hidden?: boolean
+  orderNo?: number
 }
 
 export interface AppMenu {
-    name: string
-    path: string
-    icon?: string
-    disabled?: boolean
-    children?: AppMenu[]
-    orderNo?: number
-    meta?: Partial<RouteMeta>
-    hideMenu?: boolean
+  name: string
+  path: string
+  icon?: string
+  disabled?: boolean
+  children?: AppMenu[]
+  orderNo?: number
+  meta?: Partial<RouteMeta>
+  hideMenu?: boolean
 }
 
 export interface AppMenuModule {
-    orderNo?: number
-    menu: AppMenu
+  orderNo?: number
+  menu: AppMenu
 }
