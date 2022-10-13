3817183dd559d4ab11e811d59751ff620e2a95c9
baimingxuan <354372738@qq.com>
2022-10-13 21:12:44 +0800

    pref: update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,20 +1,14 @@
-/**
- * @description: get environment variables
- */
- export function getEnv(): string {
+  // Get environment variables
+  export function getEnv(): string {
     return import.meta.env.MODE
   }
   
-  /**
-   * @description: is it a development mode
-   */
+  // It is a development mode
   export function isDevMode(): boolean {
     return import.meta.env.DEV
   }
   
-  /**
-   * @description: is it a production mode
-   */
+  // It is a production mode
   export function isProdMode(): boolean {
     return import.meta.env.PROD
   }
