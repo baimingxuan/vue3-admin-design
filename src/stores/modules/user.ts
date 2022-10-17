82174a02dd8d599ecf2f5e9e74e84755c7df7044
baimingxuan <354372738@qq.com>
2022-10-17 19:48:51 +0800

    feat: update app-user store

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -1,7 +1,11 @@
-import type { UserInfo } from '@/interfaces'
+import type { LoginParams, UserInfo } from '@/interfaces'
 
 import { defineStore } from 'pinia'
 import { stores } from '../index'
+import { router } from '@/router'
+import { loginApi, getUserInfo } from '@/api'
+import { TOKEN_KEY, USER_INFO_KEY } from '@/enums/cacheEnum'
+import { setAuthCache, getAuthCache } from '@/utils/auth'
 
 interface UserState {
   userInfo: Nullable<UserInfo>
@@ -18,11 +22,11 @@ export const useUserStore = defineStore('app-user', {
     lastUpdateTime: 0
   }),
   getters: {
-    getUserInfo() {
-      return this.userInfo || {}
+    getUserInfo(): UserInfo {
+      return this.userInfo || getAuthCache<UserInfo>(USER_INFO_KEY) || {}
     },
-    getToken() {
-      return this.token
+    getToken(): string {
+      return this.token || getAuthCache<string>(TOKEN_KEY)
     },
     getSessionTimeout(): boolean {
       return !!this.sessionTimeout
@@ -32,7 +36,64 @@ export const useUserStore = defineStore('app-user', {
     }
   },
   actions: {
+    setToken(token: string | undefined) {
+      this.token = token ? token : ''
+      setAuthCache(TOKEN_KEY, token)
+    },
+    setUserInfo(info: UserInfo | null) {
+      this.userInfo = info
+      this.lastUpdateTime = new Date().getTime()
+      setAuthCache(USER_INFO_KEY, info)
+    },
+    setSessionTimeout(flag: boolean) {
+      this.sessionTimeout = flag
+    },
+    resetState() {
+      this.userInfo = null
+      this.token = undefined
+      this.sessionTimeout = false
+      this.lastUpdateTime = 0
+    },
+    async login(
+      params: LoginParams & {
+        goHome?: boolean
+      }
+    ): Promise<UserInfo | null> {
+      try {
+        const { goHome = true, ...loginParams } = params
+        const data = await loginApi(loginParams)
+        const { token } = data
 
+        // Save token
+        this.setToken(token)
+        return this.afterLoginAction(goHome)
+      } catch (error) {
+        return Promise.reject(error)
+      }
+    },
+    async afterLoginAction(goHome?: boolean): Promise<UserInfo | null> {
+      if (!this.getToken) return null
+
+      const userInfo = await this.getUserInfoAction()
+
+      const sessionTimeout = this.sessionTimeout
+      if (sessionTimeout) {
+        this.setSessionTimeout(false)
+      } else {
+        goHome && (await router.replace(userInfo?.homePath || '/home'))
+      }
+
+      return userInfo
+    },
+    async getUserInfoAction(): Promise<UserInfo | null> {
+      if (!this.getToken) return null
+
+      const userInfo = await getUserInfo()
+
+      this.setUserInfo(userInfo)
+      
+      return userInfo
+    }
   }
 })
 
