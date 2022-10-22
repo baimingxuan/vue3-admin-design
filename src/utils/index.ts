56a6fdd9666108c9c9fff2f203da2a4c03cb417b
baimingxuan <354372738@qq.com>
2022-10-22 15:17:47 +0800

    feat: Add openWindow func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -37,3 +37,20 @@ export function getRawRoute(route: RouteLocationNormalized): RouteLocationNormal
       : undefined) as RouteRecordNormalized[]
   }
 }
+
+export function openWindow(
+  url: string,
+  opt?: { 
+    target?: TargetContext | string
+    noopener?: boolean
+    noreferrer?: boolean
+  }
+) {
+  const { target = '__blank', noopener = true, noreferrer = true } = opt || {}
+  const feature: string[] = []
+
+  noopener && feature.push('noopener=yes')
+  noreferrer && feature.push('noreferrer=yes')
+
+  window.open(url, target, feature.join(','))
+}
