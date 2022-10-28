190adbeb82949958b3c41434653ab72b490a290a
baimingxuan <354372738@qq.com>
2022-10-28 17:01:53 +0800

    feat: Update Exception routes

---
+++ src/enums/exceptionEnum.ts
@@ -0,0 +1,10 @@
+export enum ExceptionEnum {
+  // page not access
+  PAGE_NOT_ACCESS = 403,
+
+  // page not found
+  PAGE_NOT_FOUND = 404,
+
+  // server error
+  SERVER_ERROR = 500
+}
--- src/router/routes/modules/exception.ts
+++ src/router/routes/modules/exception.ts
@@ -1,35 +1,53 @@
 import type { AppRoute } from '../../types'
+import { ExceptionEnum } from '@/enums/exceptionEnum'
 import Layout from '@/layout/index.vue'
 
 // exception module page
 const ExceptionRoute: AppRoute = {
-    path: '/exception',
-    name: 'Exception',
-    component: Layout,
-    redirect: '/exception/page-401',
-    meta: {
-        title: '异常页面',
-        icon: 'bug',
-        orderNo: 11
+  path: '/exception',
+  name: 'ExceptionPage',
+  component: Layout,
+  redirect: '/exception/page-403',
+  meta: {
+    title: '异常页面',
+    icon: 'bug',
+    orderNo: 11
+  },
+  children: [
+    {
+      path: 'page-403',
+      name: 'PageNotAccess',
+      component: () => import('@/views/exception/index'),
+      props: {
+        status: ExceptionEnum.PAGE_NOT_ACCESS
+      },
+      meta: {
+        title: '403页面'
+      }
     },
-    children: [
-        {
-            path: 'page-401',
-            name: 'Page401',
-            component: () => import('@/views/home.vue'),
-            meta: {
-                title: '401页面'
-            }
-        },
-        {
-            path: 'page-404',
-            name: 'Page404',
-            component: () => import('@/views/home.vue'),
-            meta: {
-                title: '404页面'
-            }
-        }
-    ]
+    {
+      path: 'page-404',
+      name: 'PageNotFound',
+      component: () => import('@/views/exception/index'),
+      props: {
+        status: ExceptionEnum.PAGE_NOT_FOUND
+      },
+      meta: {
+        title: '404页面'
+      }
+    },
+    {
+      path: 'page-500',
+      name: 'ServiceError',
+      component: () => import('@/views/exception/index'),
+      props: {
+        status: ExceptionEnum.SERVER_ERROR
+      },
+      meta: {
+        title: '500页面'
+      }
+    }
+  ]
 }
 
 export default ExceptionRoute
