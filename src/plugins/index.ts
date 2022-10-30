137f9165ae1519c6c20b0162e894eab80c7616b8
baimingxuan <354372738@qq.com>
2022-10-30 21:04:54 +0800

    feat: Add plugins module

---
+++ src/plugins/index.ts
@@ -0,0 +1,16 @@
+import type { App } from 'vue'
+
+import VMdEditor from '@kangc/v-md-editor'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
+import '@kangc/v-md-editor/lib/theme/style/github.css'
+
+import hljs from 'highlight.js'
+
+VMdEditor.use(githubTheme, {
+  Hljs: hljs
+})
+
+export function setupPlugins(app: App<Element>) {
+  app.use(VMdEditor)
+}
