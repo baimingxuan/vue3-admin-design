e3d17d6580bea8b670ced1ac13d2db6a40a722cc
baimingxuan <354372738@qq.com>
2022-10-31 21:48:20 +0800

    feat: update content style

--- src/views/video/video-player.tsx
+++ src/views/video/video-player.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, ref } from 'vue'
+import { Row as AntdRow, Col as AntdCol, Card as AntdCard } from 'ant-design-vue'
 import { PageWrapper } from '@/components/Page'
 import { VideoPlayer } from '@videojs-player/vue'
 import 'video.js/dist/video-js.css'
@@ -14,11 +15,26 @@ export default defineComponent({
             <p>Vue-Video-Player: 一个使用Vue对video.js进行二次封装的视频播放插件, 适用于目前主流网络视频的播放。</p>
             <p>github地址: 立即访问 </p>
           </>,
-          default: () => <VideoPlayer
-              src="https://cdn.jsdelivr.net/gh/baimingxuan/media-store/videos/houlang.mp4"
-              controls
-              loop={false}
-            />
+          default: () => <AntdRow gutter={12}>
+              <AntdCol span={12}>
+                <AntdCard title='传统视频播放器' bordered={false} hoverable={true}>
+                  <video 
+                    src='https://cdn.jsdelivr.net/gh/baimingxuan/media-store/videos/houlang.mp4'
+                    controls
+                    style='outline: none;'
+                  />
+                </AntdCard>
+              </AntdCol>
+              <AntdCol span={12}>
+                <AntdCard title='视频播放插件' bordered={false} hoverable={true}>
+                  <VideoPlayer
+                    src='https://cdn.jsdelivr.net/gh/baimingxuan/media-store/videos/houlang.mp4'
+                    controls
+                    loop={false}
+                  />
+                </AntdCard>
+              </AntdCol>
+            </AntdRow>
         }}
       </PageWrapper>
     )
