b56be30271d247b99b237ef715184b612b7fede0
baimingxuan <354372738@qq.com>
2022-11-01 17:40:19 +0800

    feat: update video-player plugin

--- src/settings/websiteSetting.ts
+++ src/settings/websiteSetting.ts
@@ -1,2 +1,8 @@
 // Github repo url
 export const GITHUB_URL = 'https://github.com/baimingxuan/vue3-admin-design'
+
+// Video player plugin src
+export const VIDEO_PLUGIN_SRC = 'https://github.com/surmon-china/videojs-player'
+
+// Video resource src
+export const VIDEO_RES_SRC = 'https://cdn.jsdelivr.net/gh/baimingxuan/media-store/videos/houlang.mp4'
--- src/views/video/video-player.tsx
+++ src/views/video/video-player.tsx
@@ -1,36 +1,45 @@
-import { defineComponent, ref } from 'vue'
-import { Row as AntdRow, Col as AntdCol, Card as AntdCard } from 'ant-design-vue'
+import { defineComponent } from 'vue'
+import { Row as AntdRow, Col as AntdCol, Card as AntdCard, Button as AntdButton } from 'ant-design-vue'
 import { PageWrapper } from '@/components/Page'
+import { VIDEO_RES_SRC, VIDEO_PLUGIN_SRC } from '@/settings/websiteSetting'
+import { openWindow } from '@/utils'
 import { VideoPlayer } from '@videojs-player/vue'
 import 'video.js/dist/video-js.css'
 
 export default defineComponent({
   name: 'VideoPlayer',
   setup() {
-
+    function openGithub() {
+      openWindow(VIDEO_PLUGIN_SRC)
+    }
+    
     return () => (
       <PageWrapper name='视频播放器'>
         {{
           header: () => <>
-            <p>Vue-Video-Player: 一个使用Vue对video.js进行二次封装的视频播放插件, 适用于目前主流网络视频的播放。</p>
-            <p>github地址: 立即访问 </p>
+            <p>videojs-player: 一个支持 vue3 的 video.js 视频播放器组件, 适用于目前主流网络视频的播放。</p>
+            <p>github源码:<AntdButton type='link' onClick={openGithub}>立即访问</AntdButton></p>
           </>,
           default: () => <AntdRow gutter={12}>
               <AntdCol span={12}>
-                <AntdCard title='传统视频播放器' bordered={false} hoverable={true}>
+                <AntdCard title='传统视频播放器' bordered={false}>
                   <video 
-                    src='https://cdn.jsdelivr.net/gh/baimingxuan/media-store/videos/houlang.mp4'
+                    src={VIDEO_RES_SRC}
                     controls
-                    style='outline: none;'
+                    style='width: 100%; outline: none;'
                   />
                 </AntdCard>
               </AntdCol>
               <AntdCol span={12}>
-                <AntdCard title='视频播放插件' bordered={false} hoverable={true}>
+                <AntdCard title='视频播放插件' bordered={false}>
                   <VideoPlayer
-                    src='https://cdn.jsdelivr.net/gh/baimingxuan/media-store/videos/houlang.mp4'
+                    src={VIDEO_RES_SRC}
+                    playbackRates={[0.5, 1.0, 1.5, 2.0]}
                     controls
+                    fluid
                     loop={false}
+                    preload='auto'
+                    aspectRatio='16:9'
                   />
                 </AntdCard>
               </AntdCol>
