0458cd2a5985582fcaaa76bab9cbd527dafb2f90
baimingxuan <354372738@qq.com>
2022-11-27 22:09:27 +0800

    feat: Add excel data.ts

---
+++ src/views/excel/data.ts
@@ -0,0 +1,58 @@
+export const tableData = [
+  {
+    key: 1001,
+    name: '张三',
+    sex: '男',
+    phone: 15266001235,
+    education: '高中',
+    hobby: '羽毛球、篮球、听歌、阅读'
+  },
+  {
+    key: 1002,
+    name: '李四',
+    sex: '男',
+    phone: 15266006621,
+    education: '大专',
+    hobby: '乒乓球、排球、游泳'
+  },
+  {
+    key: 1003,
+    name: '王五',
+    sex: '男',
+    phone: 15264848125,
+    education: '大学',
+    hobby: '旱冰、滑雪、跳高、打游戏'
+  },
+  {
+    key: 1004,
+    name: '辛八',
+    sex: '男',
+    phone: 15248491001,
+    education: '初中',
+    hobby: '网球、篮球、跳伞'
+  },
+  {
+    key: 1005,
+    name: '刘二',
+    sex: '女',
+    phone: 15248411021,
+    education: '大学',
+    hobby: '滑翔、游泳、篮球、看电影'
+  },
+  {
+    key: 1006,
+    name: '赵七',
+    sex: '男',
+    phone: 15298621500,
+    education: '大专',
+    hobby: '游泳、篮球、潜水'
+  },
+  {
+    key: 1007,
+    name: '杨一',
+    sex: '女',
+    phone: 15267499461,
+    education: '高中',
+    hobby: '冲浪、上网、看书、打游戏'
+  }
+]
