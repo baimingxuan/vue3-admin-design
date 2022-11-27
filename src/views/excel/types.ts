da34f20fb30b641f7e5de6fc3692c47db5ca04ae
baimingxuan <354372738@qq.com>
2022-11-27 22:08:50 +0800

    feat: Add export-excel page and completed

--- src/views/excel/types.ts
+++ src/views/excel/types.ts
@@ -1,5 +1,7 @@
 import type { BookType } from 'xlsx'
 
+export type ImportType = 'base64' | 'binary' | 'string' | 'buffer' | 'array' | 'file'
+
 export interface DataToSheet<T = any> {
   data: T[]
   header: T[]
