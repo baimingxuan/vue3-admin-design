9bbb6fb26495907819c6e5ca3498a542a232e48f
baimingxuan <354372738@qq.com>
2022-12-28 22:47:37 +0800

    feat: Add VueDrr component

---
+++ src/components/VueDrr/src/index.tsx
@@ -0,0 +1,50 @@
+import { CSSProperties } from 'vue'
+import { defineComponent, reactive, computed } from 'vue'
+import { props } from './props'
+
+export default defineComponent({
+  name: 'VueDrr',
+  props,
+  setup(props) {
+    const config = reactive({
+      top: props.y,
+      left: props.x,
+      width: props.w,
+      height: props.h,
+      rotateAngle: props.angle,
+      enabled: props.active,
+      zIndex: props.z,
+      resizing: false,
+      dragging: false,
+      rotating: false,
+      handle: null,
+      parentW: 9999,
+      parentH: 9999,
+      mouseX: 0,
+      mouseY: 0,
+      lastMouseX: 0,
+      lastMouseY: 0,
+      mouseOffX: 0,
+      mouseOffY: 0,
+      elmX: 0,
+      elmY: 0,
+      elmW: 0,
+      elmH: 0
+    })
+
+    const getWrapStyle = computed((): CSSProperties => ({
+      top: config.top + 'px',
+      left: config.left + 'px',
+      width: config.width + 'px',
+      height: config.height + 'px',
+      transform: 'rotate(' + config.rotateAngle + 'deg)',
+      zIndex: config.zIndex,
+      overflowY: props.overflowY,
+      overflowX: ''
+    }))
+
+    return () => (
+      <div class='drr-container'></div>
+    )
+  }
+})
---
+++ src/components/VueDrr/src/props.ts
@@ -0,0 +1,126 @@
+type Overflow = 'visible' | 'hidden' | 'clip' | 'scroll' | 'auto'
+
+export const props = {
+  active: {
+    type: Boolean,
+    default: false
+  },
+  draggable: {
+    type: Boolean,
+    default: true
+  },
+  resizable: {
+    type: Boolean,
+    default: true
+  },
+  rotatable: {
+    type: Boolean,
+    default: true
+  },
+  canDeactive: {
+    type: Boolean,
+    default: true
+  },
+  w: {
+    type: [Number],
+    default: 200
+  },
+  h: {
+    type: [Number, String],
+    default: 28,
+    validator: function(val) {
+      const valid = (typeof val === 'string') ? val === 'auto' : val >= 0
+      return valid
+    }
+  },
+  ratio: {
+    type: [Number, String]
+  },
+  contains: {
+    type: Boolean,
+    default: false
+  },
+  minw: {
+    type: Number,
+    default: 28,
+    validator(val) {
+      return val > 0
+    }
+  },
+  minh: {
+    type: Number,
+    default: 28,
+    validator(val) {
+      return val > 0
+    }
+  },
+  angle: {
+    type: Number,
+    default: 0,
+    validator(val) {
+      return typeof val === 'number'
+    }
+  },
+  x: {
+    type: Number,
+    default: 0,
+    validator(val) {
+      return typeof val === 'number'
+    }
+  },
+  y: {
+    type: Number,
+    default: 0,
+    validator(val) {
+      return typeof val === 'number'
+    }
+  },
+  z: {
+    type: [String, Number],
+    default: 'auto',
+    validator: function(val) {
+      const valid = (typeof val === 'string') ? val === 'auto' : val >= 0
+      return valid
+    }
+  },
+  handles: {
+    type: Array,
+    default() {
+      return ['n', 'e', 's', 'w', 'nw', 'ne', 'se', 'sw']
+    },
+    validator: function(val) {
+      const s = new Set(['n', 'e', 's', 'w', 'nw', 'ne', 'se', 'sw'])
+
+      return new Set(val.filter(h => s.has(h))).size === val.length
+    }
+  },
+  dragHandle: {
+    type: String,
+    default: null
+  },
+  dragCancel: {
+    type: String,
+    default: null
+  },
+  axis: {
+    type: String,
+    default: 'both',
+    validator(val) {
+      return ['x', 'y', 'both'].indexOf(val) !== -1
+    }
+  },
+  grid: {
+    type: Array,
+    default() {
+      return [1, 1]
+    }
+  },
+  parent: {
+    type: Boolean,
+    default: false
+  },
+  overflowY: {
+    type: String,
+    default: ''
+  }
+}
