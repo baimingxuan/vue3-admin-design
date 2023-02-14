5d22c23823e88fb4bd12996479349eb0e7dbe9e1
baimingxuan <354372738@qq.com>
2023-02-14 15:16:26 +0800

    feat: Update treeHelper.ts

--- src/utils/helper/treeHelper.ts
+++ src/utils/helper/treeHelper.ts
@@ -40,6 +40,40 @@ export function findPath<T = any>(
   return null
 }
 
+export function filter<T = any>(
+  tree: T[],
+  func: (n: T) => boolean,
+  config: Partial<TreeHelperConfig> = {}
+): T[] {
+  config = getConfig(config)
+  const children = config.children as string
+  function listFilter(list: T[]) {
+    return list
+      .map((node: any) => ({ ...node }))
+      .filter((node) => {
+        node[children] = node[children] && listFilter(node[children])
+        return func(node) || (node[children] && node[children].length)
+      })
+  }
+  return listFilter(tree)
+}
+
+export function forEach<T = any>(
+  tree: T[],
+  func: (n: T) => any,
+  config: Partial<TreeHelperConfig> = {}
+): void {
+  config = getConfig(config)
+  const list: any[] = [...tree]
+  const { children } = config
+  for (let i = 0; i < list.length; i++) {
+    //func 返回true就终止遍历，避免大量节点场景下无意义循环，引起浏览器卡顿
+    if (func(list[i])) return
+    
+    children && list[i][children] && list.splice(i + 1, 0, ...list[i][children])
+  }
+}
+
 // Extract tree specified structure
 export function treeMap<T = any>(treeData: T[], opt: { children?: string; conversion: Fn }): T[] {
   return treeData.map(item => treeMapEach(item, opt))
