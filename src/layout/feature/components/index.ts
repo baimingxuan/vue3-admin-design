757a270ea52f653d87273a4f612d8887f40f4800
baimingxuan <354372738@qq.com>
2023-02-14 16:23:57 +0800

    chore: Del Search component

--- src/layout/feature/components/index.ts
+++ src/layout/feature/components/index.ts
@@ -1,8 +1,7 @@
-import Search from './Search'
 import FullScreen from './FullScreen'
 import LocalePicker from './LocalePicker'
 import DocLink from './DocLink'
 import GithubLink from './GithubLink'
 import UserDropdown from './UserDropdown'
 
-export { Search, FullScreen, LocalePicker, DocLink, GithubLink, UserDropdown }
+export { FullScreen, LocalePicker, DocLink, GithubLink, UserDropdown }
