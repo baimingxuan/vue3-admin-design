5a4528dd3d0b74b8e11f28bbaae10223c0cd88eb
baimingxuan <354372738@qq.com>
2023-02-20 11:04:58 +0800

    chore: Del AppModeSwitch compo

--- src/layout/setting/components/index.ts
+++ src/layout/setting/components/index.ts
@@ -1,8 +1,5 @@
-import { withInstall } from '@/utils'
 import { ImportAsyncComponent } from '@/utils/factory/ImportAsyncComponent'
-import appModeSwitch from './AppModeSwitch.vue'
 
-export const AppModeSwitch = withInstall(appModeSwitch)
 export const MenuThemeRadio = ImportAsyncComponent(() => import('./MenuThemeRadio.vue'))
 export const MenuTypePicker = ImportAsyncComponent(() => import('./MenuTypePicker.vue'))
 export const ThemeColorPicker = ImportAsyncComponent(() => import('./ThemeColorPicker.vue'))
