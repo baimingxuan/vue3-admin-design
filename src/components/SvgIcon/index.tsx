30427667758988402d0de25991423cc68bcc1f19
baimingxuan <354372738@qq.com>
2023-08-08 21:09:59 +0800

    refactor: Refactored SvgIcon compo

---
+++ src/components/SvgIcon/index.tsx
@@ -0,0 +1,38 @@
+  import type { CSSProperties } from 'vue'
+  import { defineComponent, computed, unref } from 'vue'
+  import styles from './index.module.less'
+
+  export default defineComponent({
+    name: 'SvgIcon',
+    props: {
+      prefix: {
+        type: String,
+        default: 'icon'
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      size: {
+        type: [Number, String],
+        default: 16
+      }
+    },
+    setup(props) {
+      const symbolId = computed(() => `#${props.prefix}-${props.name}`)
+      const iconStyle = computed((): CSSProperties => {
+        let size = `${props.size}`
+        size = `${size.replace('px', '')}px`
+        return {
+          width: size,
+          height: size
+        }
+      })
+
+      return () => (
+        <svg class={styles['svg-icon']} style={unref(iconStyle)} aria-hidden="true">
+          <use href={unref(symbolId)} />
+        </svg>
+      )
+    }
+  })
